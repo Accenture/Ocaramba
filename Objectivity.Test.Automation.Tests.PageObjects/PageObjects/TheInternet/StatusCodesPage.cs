047697faaa41ad2bccb8866a427808f4d5497715
pawelstryczek <pawel.stryczek@gmail.com>
2015-12-07 14:05:26 +0100

    Tests for NavigateToUrl and PageSourceContainsCase

---
+++ Objectivity.Test.Automation.Tests.PageObjects/PageObjects/TheInternet/StatusCodesPage.cs
@@ -0,0 +1,37 @@
+﻿namespace Objectivity.Test.Automation.Tests.PageObjects.PageObjects.TheInternet
+{
+    using System;
+
+    using NLog;
+
+    using Objectivity.Test.Automation.Common;
+    using Objectivity.Test.Automation.Common.Extensions;
+    using Objectivity.Test.Automation.Common.Types;
+
+    /// <summary>
+    /// The status codes page object.
+    /// </summary>
+    public class StatusCodesPage : ProjectPageBase
+    {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+        private readonly ElementLocator
+            statusCodeHeader = new ElementLocator(Locator.XPath, "//h3[text()='Status Codes']");
+
+        public StatusCodesPage(DriverContext driverContext)
+            : base(driverContext)
+        {
+        }
+
+        public bool IsStatusCodesPageDisplayed()
+        {
+            Logger.Info("Check if Status Codes page is displayed.");
+            return this.Driver.IsElementPresent(this.statusCodeHeader, BaseConfiguration.MediumTimeout);
+        }
+
+        public bool IsTextExistedInPageSource(string text)
+        {
+            return this.Driver.PageSourceContainsCase(text, BaseConfiguration.MediumTimeout, false);
+        }
+    }
+}
