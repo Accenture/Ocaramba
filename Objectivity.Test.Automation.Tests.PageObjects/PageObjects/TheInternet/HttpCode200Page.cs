7228d9afef9ca8659fdca6af3af14bf56fa9c417
pawelstryczek <pawel.stryczek@gmail.com>
2015-12-07 15:14:25 +0100

    Added JavaScriptClickTest

---
+++ Objectivity.Test.Automation.Tests.PageObjects/PageObjects/TheInternet/HttpCode200Page.cs
@@ -0,0 +1,22 @@
+﻿namespace Objectivity.Test.Automation.Tests.PageObjects.PageObjects.TheInternet
+{
+    using Objectivity.Test.Automation.Common;
+    using Objectivity.Test.Automation.Common.Extensions;
+    using Objectivity.Test.Automation.Common.Types;
+
+    public class HTTPCode200Page : ProjectPageBase
+    {
+        private readonly ElementLocator
+            HttpCode200Header = new ElementLocator(Locator.XPath, "//*[contains(text(),'This page returned a 200 status code.')]");
+
+        public HTTPCode200Page(DriverContext driverContext)
+            : base(driverContext)
+        {
+        }
+
+        public bool IsHTTPCode200PageIsDisplayed()
+        {
+            return this.Driver.IsElementPresent(this.HttpCode200Header, BaseConfiguration.MediumTimeout);
+        }
+    }
+}
