a6dcac87b223b73528aa30b829a4e087a1ceebd7
Tomasz Bien <tbien@objectivity.co.uk>
2015-12-07 11:51:41 +0100

    add IsPageTitle method

--- Objectivity.Test.Automation.Tests.NUnit/Tests/tbien.cs
+++ Objectivity.Test.Automation.Tests.NUnit/Tests/tbien.cs
@@ -8,12 +8,15 @@ namespace Objectivity.Test.Automation.Tests.NUnit.Tests
         [Test]
         public void SwitchToWindowTest()
         {
+            const string pageTitle = "New Window";
+
             var newWindowPage = new InternetPage(this.DriverContext)
                 .OpenHomePage()
                 .GoToMultipleWindowsPage()
                 .OpenNewWindowPage();
 
-            Assert.True(newWindowPage.IsNewWindowH3TextVisible("New Window"), "text is not equal to {0}", "New Window");
+            Assert.True(newWindowPage.IsPageTile(pageTitle), "wrong page title, should be {0}", pageTitle);
+            Assert.True(newWindowPage.IsNewWindowH3TextVisible(pageTitle), "text is not equal to {0}", pageTitle);
         }
     }
 }
