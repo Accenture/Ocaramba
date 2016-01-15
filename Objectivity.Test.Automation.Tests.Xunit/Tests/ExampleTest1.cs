d571d54713fc2d3e7ec05d5adc2fed6db682732c
tomasz bien <tbien@objectivity.co.uk>
2016-01-15 10:10:11 +0100

    add xunit test framework to objectoivity test framework

---
+++ Objectivity.Test.Automation.Tests.Xunit/Tests/ExampleTest1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Objectivity.Test.Automation.Common;
+using Objectivity.Test.Automation.Tests.PageObjects.PageObjects.Kendo;
+using Objectivity.Test.Automation.Tests.PageObjects.PageObjects.TheInternet;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Objectivity.Test.Automation.Tests.Xunit.Tests
+{
+
+    public class ExampleTest1 : ProjectTestBase
+    {
+
+        [Fact]
+        public void TestMethod1()
+        {
+            const string pageTitle = "New Window";
+
+            var newWindowPage = new InternetPage(DriverContext)
+                .OpenHomePage()
+                .GoToMultipleWindowsPage()
+                .OpenNewWindowPage();
+
+            Assert.True(newWindowPage.IsPageTile(pageTitle));
+            Assert.True(newWindowPage.IsNewWindowH3TextVisible(pageTitle));
+        }
+
+        [Fact]
+        public void ContextMenuTest()
+        {
+            const string h3Value = "Context Menu";
+            var browser = BaseConfiguration.TestBrowser;
+            if (browser.Equals(DriverContext.BrowserType.Firefox))
+            {
+                var contextMenuPage = new InternetPage(DriverContext)
+                    .OpenHomePage()
+                    .GoToContextMenuPage()
+                    .SelectTheInternetOptionFromContextMenu();
+
+                Assert.Equal("You selected a context menu", contextMenuPage.JavaScriptText);
+                Assert.True(contextMenuPage
+                    .ConfirmJavaScript()
+                    .IsH3ElementEqualsToExpected(h3Value));
+            }
+        }
+    }
+}
+
+
---
+++ Objectivity.Test.Automation.Tests.Xunit/Tests/ExampleTest2.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Objectivity.Test.Automation.Common;
+using Objectivity.Test.Automation.Tests.PageObjects.PageObjects.Kendo;
+using Objectivity.Test.Automation.Tests.PageObjects.PageObjects.TheInternet;
+using Xunit;
+
+namespace Objectivity.Test.Automation.Tests.Xunit.Tests
+{
+    public class ExampleTest2 : ProjectTestBase
+    {
+
+        [Fact]
+        public void NestedFramesTest()
+        {
+            var nestedFramesPage = new InternetPage(DriverContext)
+                .OpenHomePage()
+                .GoToNestedFramesPage()
+                .SwitchToFrame("frame-top");
+
+            nestedFramesPage.SwitchToFrame("frame-left");
+            Assert.Equal("LEFT", nestedFramesPage.LeftBody);
+
+            nestedFramesPage.SwitchToParentFrame().SwitchToFrame("frame-middle");
+            Assert.Equal("MIDDLE", nestedFramesPage.MiddleBody);
+
+            nestedFramesPage.SwitchToParentFrame().SwitchToFrame("frame-right");
+            Assert.Equal("RIGHT", nestedFramesPage.RightBody);
+
+            nestedFramesPage.ReturnToDefaultContent().SwitchToFrame("frame-bottom");
+            Assert.Equal("BOTTOM", nestedFramesPage.BottomBody);
+        }
+
+        [Fact]
+        public void KendoOpenCloseComboboxTest()
+        {
+            var homePage = new KendoComboBoxPage(DriverContext);
+            homePage.Open();
+
+            homePage.OpenFabricComboBox();
+            Assert.True(homePage.IsFabricComboBoxExpanded());
+
+            homePage.CloseFabricComboBox();
+            Assert.False(homePage.IsFabricComboBoxExpanded());
+        }
+
+    }
+}
