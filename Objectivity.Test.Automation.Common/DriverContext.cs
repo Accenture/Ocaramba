78c802ae1b40d8d0febb01367ab4b3853e3063d7
Jakub Raczek <jraczek@objectivity.co.uk>
2017-07-20 09:12:24 +0200

    Fixed small build issue

--- Objectivity.Test.Automation.Common/DriverContext.cs
+++ Objectivity.Test.Automation.Common/DriverContext.cs
@@ -409,51 +409,6 @@ namespace Objectivity.Test.Automation.Common
         }
 
         /// <summary>
-        /// Set web driver capabilities.
-        /// </summary>
-        /// <returns>Instance with set web driver capabilities.</returns>
-        public DesiredCapabilities SetCapabilities()
-        {
-            DesiredCapabilities capabilities = new DesiredCapabilities();
-
-            switch (BaseConfiguration.TestBrowserCapabilities)
-            {
-                case BrowserType.Firefox:
-                    capabilities = DesiredCapabilities.Firefox();
-                    capabilities.SetCapability(FirefoxDriver.ProfileCapabilityName, this.FirefoxProfile.ToBase64String());
-                    break;
-                case BrowserType.InternetExplorer:
-                    capabilities = DesiredCapabilities.InternetExplorer();
-                    break;
-                case BrowserType.Chrome:
-                    capabilities = DesiredCapabilities.Chrome();
-                    break;
-                case BrowserType.Safari:
-                    capabilities = DesiredCapabilities.Safari();
-                    break;
-                default:
-                    throw new NotSupportedException(
-                        string.Format(CultureInfo.CurrentCulture, "Driver {0} is not supported with Selenium Grid", BaseConfiguration.TestBrowser));
-            }
-
-            var driverCapabilitiesConf = ConfigurationManager.GetSection("DriverCapabilities") as NameValueCollection;
-
-            // if there are any capability
-            if (driverCapabilitiesConf != null)
-            {
-                // loop through all of them
-                for (var i = 0; i < driverCapabilitiesConf.Count; i++)
-                {
-                    string value = driverCapabilitiesConf.GetValues(i)[0];
-                    Logger.Trace(CultureInfo.CurrentCulture, "Adding driver capability {0}", driverCapabilitiesConf.GetKey(i));
-                    capabilities.SetCapability(driverCapabilitiesConf.GetKey(i), value);
-                }
-            }
-
-            return capabilities;
-        }
-
-        /// <summary>
         /// Starts the specified Driver.
         /// </summary>
         /// <exception cref="NotSupportedException">When driver not supported</exception>
