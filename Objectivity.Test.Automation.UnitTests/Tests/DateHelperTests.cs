d8651986c58d0dac14ffb4e3ad43fbd45c057e0d
Jakub Raczek <jraczek@objectivity.co.uk>
2018-06-11 10:19:12 +0200

    Added unit tests

---
+++ Objectivity.Test.Automation.UnitTests/Tests/DateHelperTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+using NUnit.Framework;
+using Objectivity.Test.Automation.Common.Helpers;
+
+namespace Objectivity.Test.Automation.UnitTests.Tests
+{
+    [TestFixture()]
+    [TestFixture, Parallelizable(ParallelScope.Self)]
+    public class DateHelperTests
+    {
+        [Test()]
+        public void TomorrowDateTest()
+        {
+            Assert.AreEqual(DateTime.Now.AddDays(1).ToString("ddMMyyyy", CultureInfo.CurrentCulture), DateHelper.TomorrowDate);
+        }
+
+        [Test()]
+        public void CurrentDateTest()
+        {
+            Assert.AreEqual(DateTime.Now.ToString("dd-MM-yyyy", CultureInfo.CurrentCulture), DateHelper.CurrentDate);
+        }
+
+        [Test()]
+        public void CurrentTimeStampTest()
+        {
+            Assert.AreEqual(DateTime.Now.ToString("ddMMyyyyHHmmss", CultureInfo.CurrentCulture), DateHelper.CurrentTimeStamp);
+        }
+
+        [Test()]
+        public void GetFutureDateTest()
+        {
+            Assert.AreEqual(DateTime.Now.AddDays(3).ToString("ddMMyyyy", CultureInfo.CurrentCulture), DateHelper.GetFutureDate(3));
+        }
+    }
+}
---
+++ Objectivity.Test.Automation.UnitTests/Tests/NameHelperTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using Objectivity.Test.Automation.Common.Helpers;
+
+namespace Objectivity.Test.Automation.UnitTests.Tests
+{
+    [TestFixture()]
+    [TestFixture, Parallelizable(ParallelScope.Self)]
+    public class NameHelperTests
+    {
+        [Test()]
+        public void ShortenFileNameTest()
+        {
+            var name = "verylongfilename 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0.txt";
+            var text = NameHelper.ShortenFileName(TestContext.CurrentContext.TestDirectory, name, "_", 255);
+            Assert.IsTrue((TestContext.CurrentContext.TestDirectory + name).Length > 255);
+            text = NameHelper.ShortenFileName(TestContext.CurrentContext.TestDirectory, name, " ", 255);
+            Assert.AreEqual(255, (TestContext.CurrentContext.TestDirectory + text).Length);
+        }
+
+    }
+}
