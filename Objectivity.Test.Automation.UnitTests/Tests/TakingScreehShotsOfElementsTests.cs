ce5a86ea70ca158ad5a28941284abeb98779f90e
Jakub Raczek <jraczek@objectivity.co.uk>
2018-07-10 14:47:12 +0200

    enabled RunCodeAnalysis for all subprojects

--- Objectivity.Test.Automation.UnitTests/Tests/TakingScreehShotsOfElementsTests.cs
+++ Objectivity.Test.Automation.UnitTests/Tests/TakingScreehShotsOfElementsTests.cs
@@ -50,7 +50,7 @@ namespace Objectivity.Test.Automation.UnitTests.Tests
                     using (var imgDiff = new MagickImage())
                     {
                         img1.Compose = CompositeOperator.Src;
-                        double diff = img1.Compare(img2, new ErrorMetric(), imgDiff);
+                        img1.Compare(img2, new ErrorMetric(), imgDiff);
                         flag = img1.Equals(img2);
                         imgDiff.Write(TestContext.CurrentContext.TestDirectory + BaseConfiguration.ScreenShotFolder + "\\" + BaseConfiguration.TestBrowser + "TextWithinIFrameDIFF.png");
                     }
