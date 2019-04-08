e8bbd756f480c5d5aa856e61caf80381ac3e7b89
Jakub Raczek <jraczek@objectivity.co.uk>
2019-04-08 13:43:44 +0200

    removed unused method SetRemoteDriverOptions

--- Objectivity.Test.Automation.Common/DriverContext.cs
+++ Objectivity.Test.Automation.Common/DriverContext.cs
@@ -522,12 +522,12 @@ namespace Objectivity.Test.Automation.Common
                         case BrowserType.Iphone:
                         case BrowserType.Safari:
                             SafariOptions safariOptions = new SafariOptions();
-                            this.SetRemoteDriverOptions(driverCapabilitiesConf, settings, safariOptions);
+                            this.SetRemoteDriverBrowserOptions(driverCapabilitiesConf, settings, safariOptions);
                             this.driver = new RemoteWebDriver(BaseConfiguration.RemoteWebDriverHub, this.SetDriverOptions(safariOptions).ToCapabilities());
                             break;
                         case BrowserType.Edge:
                             EdgeOptions egEdgeOptions = new EdgeOptions();
-                            this.SetRemoteDriverOptions(driverCapabilitiesConf, settings, egEdgeOptions);
+                            this.SetRemoteDriverBrowserOptions(driverCapabilitiesConf, settings, egEdgeOptions);
                             this.driver = new RemoteWebDriver(BaseConfiguration.RemoteWebDriverHub, this.SetDriverOptions(egEdgeOptions).ToCapabilities());
                             break;
                         case BrowserType.IE:
