da06ac40d811ca5c4acdd72ac64baa891ce1f92e
Jakub Raczek <jraczek@objectivity.co.uk>
2019-04-19 08:56:26 +0200

    Fixed specflow tests

--- Ocaramba.Tests.Features/Properties/AssemblyInfo.cs
+++ Ocaramba.Tests.Features/Properties/AssemblyInfo.cs
@@ -28,11 +28,11 @@ using NUnit.Framework;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Objectivity.Test.Automation.Common.Sample")]
+[assembly: AssemblyTitle("Ocaramba.Sample")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Objectivity.Test.Automation.Common.Sample")]
+[assembly: AssemblyProduct("Ocaramba.Sample")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
