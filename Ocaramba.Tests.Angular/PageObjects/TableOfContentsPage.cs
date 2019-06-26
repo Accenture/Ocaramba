a35f45271ea0332161cb8024d1caa7897903b8f8
Jakub Raczek <jraczek@objectivity.co.uk>
2019-06-26 08:30:39 +0200

    Added Angular test project

---
+++ Ocaramba.Tests.Angular/PageObjects/TableOfContentsPage.cs
@@ -0,0 +1,30 @@
+﻿using Ocaramba.Tests.PageObjects;
+
+namespace Ocaramba.Tests.Angular.PageObjects
+{
+    using NLog;
+    using Ocaramba.Types;
+    using Ocaramba;
+    using Ocaramba.Extensions;
+
+    public class TableOfContentsPage : ProjectPageBase
+    {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+        /// <summary>
+        /// Locators for elements
+        /// </summary>
+        private readonly ElementLocator
+            ProtractorApi = new ElementLocator(Locator.CssSelector, "ul[class='ng-scope']>li>a[href='#/api']");
+
+        public TableOfContentsPage(DriverContext driverContext) : base(driverContext)
+        {
+        }
+
+        public ProtractorApiPage ClickProtractorApi()
+        {
+            this.Driver.GetElement(this.ProtractorApi).Click();
+            return new ProtractorApiPage(this.DriverContext);
+        }
+    }
+}
