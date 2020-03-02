20d89a35bd4c12f08ffbe72ba81c4aa10d8d1aed
Jakub Raczek <jraczek@objectivity.co.uk>
2020-03-02 11:44:47 +0100

    Added ExecutingTestsOnDocker

---
+++ ExecutingTestsOnDocker.ps1
@@ -0,0 +1,13 @@
+docker-switch-linux
+         
+docker info
+         
+docker pull ocaramba/selenium
+
+cd .\Ocaramba
+         
+docker build -t ocaramba/selenium . 
+         
+docker ps -a
+
+cd ..
