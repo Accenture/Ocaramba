c9db89f87099c22961ef7dfca812666f64445bb3
Jakub Raczek <jraczek@objectivity.co.uk>
2020-03-30 17:23:28 +0200

    Fixed name of project

--- ExecutingTestsOnWindowsAzure.ps1
+++ ExecutingTestsOnWindowsAzure.ps1
@@ -4,7 +4,7 @@ echo '********************************************NUnit tests*******************
 
 dotnet vstest .\Ocaramba.Tests.Angular\bin\Release\netcoreapp3.1\Ocaramba.Tests.Angular.dll `
 	          .\Ocaramba.Tests.NUnit\bin\Release\netcoreapp3.1\Ocaramba.Tests.NUnit.dll `
-			  .\Ocaramba.Tests.Feature\bin\Release\netcoreapp3.1\Ocaramba.Tests.Feature.dll `
+			  .\Ocaramba.Tests.Features\bin\Release\netcoreapp3.1\Ocaramba.Tests.Features.dll `
 			  .\Ocaramba.Tests.Xunit\bin\Release\netcoreapp3.1\Ocaramba.Tests.Xunit.dll `
 			  .\Ocaramba.Tests.MsTest\bin\Release\netcoreapp3.1\Ocaramba.Tests.MsTest.dll `
 	          .\Ocaramba.UnitTests\bin\Release\netcoreapp3.1\Ocaramba.UnitTests.dll `
@@ -18,7 +18,7 @@ if($lastexitcode -ne 0)
 
 dotnet vstest .\Ocaramba.Tests.Angular\bin\Release\net472\Ocaramba.Tests.Angular.dll `
 	          .\Ocaramba.Tests.NUnit\bin\Release\net472\Ocaramba.Tests.NUnit.dll `
-			  .\Ocaramba.Tests.Feature\bin\Release\net472\Ocaramba.Tests.Feature.dll `
+			  .\Ocaramba.Tests.Features\bin\Release\net472\Ocaramba.Tests.Features.dll `
 			  .\Ocaramba.Tests.Xunit\bin\Release\net472\Ocaramba.Tests.Xunit.dll `
 			  .\Ocaramba.Tests.MsTest\bin\Release\net472\Ocaramba.Tests.MsTest.dll `
 			  .\Ocaramba.UnitTests\bin\Release\net472\Ocaramba.UnitTests.dll `
