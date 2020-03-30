d318ab8e4601db37f07d2839aafc5bae993f3901
Jakub Raczek <jraczek@objectivity.co.uk>
2020-03-30 16:02:18 +0200

    Fixed paths to tests

--- ExecutingTestsOnWindowsAzure.ps1
+++ ExecutingTestsOnWindowsAzure.ps1
@@ -2,4 +2,4 @@ echo '********************************************Executing tests***************
         
 echo '********************************************NUnit tests********************************************'
 
-dotnet vstest .\Ocaramba.Tests.Angular\bin\Debug\netcoreapp3.1\Ocaramba.Tests.Angular.dll .\Ocaramba.Tests.NUnit\bin\Debug\netcoreapp3.1\Ocaramba.Tests.NUnit.dll .\Ocaramba.UnitTests\bin\Debug\netcoreapp3.1\Ocaramba.UnitTests.dll --logger:"trx;LogFileName=Ocaramba.Tests.netcoreapp3.xml"
+dotnet vstest .\Ocaramba.Tests.Angular\bin\Release\netcoreapp3.1\Ocaramba.Tests.Angular.dll .\Ocaramba.Tests.NUnit\bin\Release\netcoreapp3.1\Ocaramba.Tests.NUnit.dll .\Ocaramba.UnitTests\bin\Release\netcoreapp3.1\Ocaramba.UnitTests.dll --logger:"trx;LogFileName=Ocaramba.Tests.netcoreapp3.xml"
