7a4878d5fde71c28a77b39c969905c2d10d815cb
Jakub Raczek <jraczek@objectivity.co.uk>
2020-03-30 23:31:33 +0200

    Added TestCaseFilter

--- ExecutingTestsOnWindowsAzure.ps1
+++ ExecutingTestsOnWindowsAzure.ps1
@@ -8,9 +8,10 @@ dotnet vstest .\Ocaramba.Tests.Angular\bin\Release\netcoreapp3.1\Ocaramba.Tests.
 			  .\Ocaramba.Tests.Xunit\bin\Release\netcoreapp3.1\Ocaramba.Tests.Xunit.dll `
 			  .\Ocaramba.Tests.MsTest\bin\Release\netcoreapp3.1\Ocaramba.Tests.MsTest.dll `
 	          .\Ocaramba.UnitTests\bin\Release\netcoreapp3.1\Ocaramba.UnitTests.dll `
+			  /TestCaseFilter:"(TestCategory!=TakingScreehShots)" `
 	          --logger:"trx;LogFileName=Ocaramba.Tests.netcoreapp.xml"
 
-echo '********************************************netcoreapp3 tests********************************************'
+echo '********************************************net472 tests********************************************'
 
 $vstest = (Resolve-Path "D:\a\_temp\VsTest\Microsoft.TestPlatform*\tools\net*\Common*\IDE\Extensions\TestPlatform\vstest.console.exe").ToString()
 & $vstest .\Ocaramba.Tests.Angular\bin\Release\net472\Ocaramba.Tests.Angular.dll `
@@ -19,6 +20,7 @@ $vstest = (Resolve-Path "D:\a\_temp\VsTest\Microsoft.TestPlatform*\tools\net*\Co
 			  .\Ocaramba.Tests.Xunit\bin\Release\net472\Ocaramba.Tests.Xunit.dll `
 			  .\Ocaramba.Tests.MsTest\bin\Release\net472\Ocaramba.Tests.MsTest.dll `
 	          .\Ocaramba.UnitTests\bin\Release\net472\Ocaramba.UnitTests.dll `
+			  /TestCaseFilter:"(TestCategory!=NotImplementedInCoreOrUploadDownload)" `
 	          --logger:"trx;LogFileName=Ocaramba.Tests.net472.xml"
 
 if($lastexitcode -ne 0)
