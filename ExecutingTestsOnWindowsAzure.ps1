644afb7ba7cb33eac63b3e2da20d0a8b79d2573a
Jakub Raczek <jraczek@objectivity.co.uk>
2020-03-30 16:09:40 +0200

    Ignoring last exit code for tests

--- ExecutingTestsOnWindowsAzure.ps1
+++ ExecutingTestsOnWindowsAzure.ps1
@@ -3,3 +3,8 @@ echo '********************************************Executing tests***************
 echo '********************************************NUnit tests********************************************'
 
 dotnet vstest .\Ocaramba.Tests.Angular\bin\Release\netcoreapp3.1\Ocaramba.Tests.Angular.dll .\Ocaramba.Tests.NUnit\bin\Release\netcoreapp3.1\Ocaramba.Tests.NUnit.dll .\Ocaramba.UnitTests\bin\Release\netcoreapp3.1\Ocaramba.UnitTests.dll --logger:"trx;LogFileName=Ocaramba.Tests.netcoreapp3.xml"
+
+if($lastexitcode -ne 0)
+ {
+  echo 'lastexitcode' $lastexitcode
+ }
