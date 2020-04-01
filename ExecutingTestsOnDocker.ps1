785730be6461c720745d0c2377702f7786af541d
Jakub Raczek <jraczek@objectivity.co.uk>
2020-04-01 22:38:35 +0200

    Updated running test on Docker

--- ExecutingTestsOnDocker.ps1
+++ ExecutingTestsOnDocker.ps1
@@ -4,18 +4,23 @@ docker-switch-linux
          
 docker info
          
-docker pull ocaramba/selenium
-         
 docker build -t ocaramba/selenium -f DockerfileBuild .
          
 docker run --rm -dit --name ocaramba_selenium ocaramba/selenium
 docker ps -a
+
 docker exec ocaramba_selenium sed -i '/Features/,+1 d' Ocaramba.sln
 docker exec ocaramba_selenium sed -i '/Documentation/,+5 d' Ocaramba.sln
+echo 'build Ocaramba.sln'
 docker exec ocaramba_selenium dotnet build Ocaramba.sln
+echo 'execute Ocaramba.sln tests'
 docker exec ocaramba_selenium dotnet vstest Ocaramba.Tests.NUnit/bin/Debug/netcoreapp3.1/Ocaramba.Tests.NUnit.dll  /TestCaseFilter:"(TestCategory!=NotImplementedInCoreOrUploadDownload)" --logger:"trx;LogFileName=Ocaramba.Tests.Docker.xml"
-
+echo 'Downloading Ocaramba.Tests.Docker.xml'
 docker cp  ocaramba_selenium:/Ocaramba/TestResults/Ocaramba.Tests.Docker.xml .
-
+echo 'Uploading Ocaramba.Tests.Docker.xml'
 $wc = New-Object 'System.Net.WebClient'
 $wc.UploadFile("https://ci.appveyor.com/api/testresults/xunit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path .\Ocaramba.Tests.Docker.xml))
+
+docker rm ocaramba_selenium --force
+
+
