c346aec99bb2cd8b768e8ad7b111ef8a634b922d
Jakub Raczek <jraczek@objectivity.co.uk>
2020-04-01 22:01:08 +0200

    Check if IsLinux

--- removeSubprojectFromSolution.ps1
+++ removeSubprojectFromSolution.ps1
@@ -1,6 +1,7 @@
+if ($IsLinux) {
 echo "removing Features subprojects from .\Ocaramba.sln"
 (Get-Content '.\Ocaramba.sln' -raw) -replace [regex]('.*?' + 'Features' + ('.*?\n' * (1 + 1))) | set-content  '.\Ocaramba.sln'
-	
+	}
 
 echo "removing Documentation subprojects from .\Ocaramba.sln"
 (Get-Content '.\Ocaramba.sln' -raw) -replace [regex]('.*?' + 'Documentation' + ('.*?\n' * (5 + 1))) | set-content  '.\Ocaramba.sln'
