4ff7e17ba7d5a490ae57a669eb4f6f0326efc33b
Jakub Raczek <jraczek@objectivity.co.uk>
2020-04-01 10:11:33 +0200

    Disabled building Features project on Linux Fixed Documentation subproject

--- ExecutingTestsOnDocker.ps1
+++ ExecutingTestsOnDocker.ps1
@@ -8,4 +8,11 @@ docker pull ocaramba/selenium
          
 docker build -t ocaramba/selenium -f DockerfileBuild .
          
+docker run --rm -dit --name ocaramba_selenium ocaramba/selenium
 docker ps -a
+docker exec ocaramba_selenium sed -i '/Features/,+1 d' Ocaramba.sln
+docker exec ocaramba_selenium sed -i '/Documentation/,+5 d' Ocaramba.sln
+docker exec ocaramba_selenium dotnet build Ocaramba.sln
+docker exec ocaramba_selenium dotnet vstest Ocaramba.Tests.NUnit/bin/Debug/netcoreapp3.1/Ocaramba.Tests.NUnit.dll  /TestCaseFilter:"(TestCategory!=NotImplementedInCoreOrUploadDownload)" --logger:"trx;LogFileName=Ocaramba.Tests.Docker.xml"
+
+docker cp  ocaramba_selenium:/Ocaramba/TestResults/Ocaramba.Tests.Docker.xml .
--- ExecutingTestsOnWindowsAzure.ps1
+++ ExecutingTestsOnWindowsAzure.ps1
@@ -14,14 +14,23 @@ dotnet vstest .\Ocaramba.Tests.Angular\bin\Release\netcoreapp3.1\Ocaramba.Tests.
 echo '********************************************net472 tests********************************************'
 
 $vstest = (Resolve-Path "D:\a\_temp\VsTest\Microsoft.TestPlatform*\tools\net*\Common*\IDE\Extensions\TestPlatform\vstest.console.exe").ToString()
-& $vstest .\Ocaramba.Tests.Angular\bin\Release\net472\Ocaramba.Tests.Angular.dll `
-	          .\Ocaramba.Tests.NUnit\bin\Release\net472\Ocaramba.Tests.NUnit.dll `
-			  .\Ocaramba.Tests.Features\bin\Release\net472\Ocaramba.Tests.Features.dll `
-			  .\Ocaramba.Tests.Xunit\bin\Release\net472\Ocaramba.Tests.Xunit.dll `
-			  .\Ocaramba.Tests.MsTest\bin\Release\net472\Ocaramba.Tests.MsTest.dll `
-	          .\Ocaramba.UnitTests\bin\Release\net472\Ocaramba.UnitTests.dll `
-			  /TestCaseFilter:"(TestCategory!=NotImplementedInCoreOrUploadDownload)" `
-	          --logger:"trx;LogFileName=Ocaramba.Tests.net472.xml"
+
+docker info
+         
+docker pull ocaramba/selenium
+         
+docker build -t ocaramba/selenium -f DockerfileBuild .
+         
+docker run --rm -dit --name ocaramba_selenium ocaramba/selenium
+docker ps -a
+docker exec ocaramba_selenium sed -i '/Features/,+1 d' Ocaramba.sln
+docker exec ocaramba_selenium sed -i '/Documentation/,+5 d' Ocaramba.sln
+docker exec ocaramba_selenium dotnet build Ocaramba.sln
+docker exec ocaramba_selenium dotnet vstest Ocaramba.Tests.NUnit/bin/Debug/netcoreapp3.1/Ocaramba.Tests.NUnit.dll  /TestCaseFilter:"(TestCategory!=NotImplementedInCoreOrUploadDownload)" --logger:"trx;LogFileName=Ocaramba.Tests.Docker.xml"
+
+docker cp  ocaramba_selenium:/Ocaramba/TestResults/Ocaramba.Tests.Docker.xml .
+
+
 
 if($lastexitcode -ne 0)
  {
