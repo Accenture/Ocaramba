81dcd23b4d7a7ac850a0d3a52c4be460d4913bc8
Jakub Raczek <jraczek@objectivity.co.uk>
2020-04-01 15:05:00 +0200

    Enabled $blockRdp = $true;

--- removeSubprojectFromSolution.ps1
+++ removeSubprojectFromSolution.ps1
@@ -1,1 +1,3 @@
-﻿(Get-Content '.\Ocaramba.sln' -raw) -replace [regex]('.*?' + 'Documentation' + ('.*?\r\n' * (5 + 1))) | set-content  '.\Ocaramba.sln'
+﻿echo "removing Documentation subprojects from .\Ocaramba.sln"
+
+(Get-Content '.\Ocaramba.sln' -raw) -replace [regex]('.*?' + 'Documentation' + ('.*?\r\n' * (5 + 1))) | set-content  '.\Ocaramba.sln'
