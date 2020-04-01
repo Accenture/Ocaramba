378be94886102a844933379cb6dbdddb6166e08a
Jakub Raczek <jraczek@objectivity.co.uk>
2020-04-01 20:57:35 +0200

    Enabled running test on linux

--- removeSubprojectFromSolution.ps1
+++ removeSubprojectFromSolution.ps1
@@ -1,3 +1,10 @@
 ﻿echo "removing Documentation subprojects from .\Ocaramba.sln"
 
-(Get-Content '.\Ocaramba.sln' -raw) -replace [regex]('.*?' + 'Documentation' + ('.*?\n' * (5 + 1))) | set-content  '.\Ocaramba.sln'
+if ($env:isWindows) {
+
+	(Get-Content '.\Ocaramba.sln' -raw) -replace [regex]('.*?' + 'Documentation' + ('.*?\n' * (5 + 1))) | set-content  '.\Ocaramba.sln'
+	
+} else {
+
+	(Get-Content '.\Ocaramba.sln' -raw) -replace [regex]('.*?' + 'Documentation' + ('.*?\r\n' * (5 + 1))) | set-content  '.\Ocaramba.sln'
+}
