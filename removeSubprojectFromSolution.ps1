d826983601f9d2276fbbc96cc9d656999a9c9ea5
Jakub Raczek <jraczek@objectivity.co.uk>
2020-04-01 21:03:48 +0200

    Fixed removing  Features subproject from Solution before build on linux

--- removeSubprojectFromSolution.ps1
+++ removeSubprojectFromSolution.ps1
@@ -1,11 +1,9 @@
-﻿echo "removing Documentation subprojects from .\Ocaramba.sln"
-
-if ($env:isWindows) {
-
-	(Get-Content '.\Ocaramba.sln' -raw) -replace [regex]('.*?' + 'Documentation' + ('.*?\n' * (5 + 1))) | set-content  '.\Ocaramba.sln'
+﻿
+if ($env:isLinux) {
+echo "removing Features subprojects from .\Ocaramba.sln"
+(Get-Content '.\Ocaramba.sln' -raw) -replace [regex]('.*?' + 'Features' + ('.*?\n' * (5 + 1))) | set-content  '.\Ocaramba.sln'
 	
-} else {
-
-	(Get-Content '.\Ocaramba.sln' -raw) -replace [regex]('.*?' + 'Documentation' + ('.*?\r\n' * (5 + 1))) | set-content  '.\Ocaramba.sln'
-	(Get-Content '.\Ocaramba.sln' -raw) -replace [regex]('.*?' + 'Features' + ('.*?\r\n' * (5 + 1))) | set-content  '.\Ocaramba.sln'
-}
+} 
+echo "removing Documentation subprojects from .\Ocaramba.sln"
+(Get-Content '.\Ocaramba.sln' -raw) -replace [regex]('.*?' + 'Documentation' + ('.*?\n' * (5 + 1))) | set-content  '.\Ocaramba.sln'
+	
