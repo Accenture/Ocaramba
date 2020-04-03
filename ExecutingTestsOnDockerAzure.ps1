c1b40b42b41b8e95250e97b49b0109384009d55b
Jakub Raczek <jraczek@objectivity.co.uk>
2020-04-03 17:58:51 +0200

    Removed WORKDIR form DockerBuild/Dockerfile

--- ExecutingTestsOnDockerAzure.ps1
+++ ExecutingTestsOnDockerAzure.ps1
@@ -1,11 +1,11 @@
 echo '********************************************Executing tests********************************************'
 docker ps        
-docker exec ocaramba_selenium sed -i '/Features/,+1 d' Ocaramba.sln
-docker exec ocaramba_selenium sed -i '/Documentation/,+5 d' Ocaramba.sln
+docker exec ocaramba_selenium sed -i '/Features/,+1 d' /Ocaramba/Ocaramba.sln
+docker exec ocaramba_selenium sed -i '/Documentation/,+5 d' /Ocaramba/Ocaramba.sln
 echo 'build Ocaramba.sln'
-docker exec ocaramba_selenium dotnet build Ocaramba.sln
+docker exec ocaramba_selenium dotnet build /Ocaramba/Ocaramba.sln
 echo 'execute Ocaramba.sln tests'
-docker exec ocaramba_selenium dotnet vstest Ocaramba.Tests.NUnit/bin/Debug/netcoreapp3.1/Ocaramba.Tests.NUnit.dll  /TestCaseFilter:"(TestCategory!=NotImplementedInCoreOrUploadDownload)" --logger:"trx;LogFileName=Ocaramba.Tests.Docker.xml"
+docker exec ocaramba_selenium dotnet vstest /Ocaramba/Ocaramba.Tests.NUnit/bin/Debug/netcoreapp3.1/Ocaramba.Tests.NUnit.dll  /TestCaseFilter:"(TestCategory!=NotImplementedInCoreOrUploadDownload)" --logger:"trx;LogFileName=Ocaramba.Tests.Docker.xml"
 echo 'Downloading Ocaramba.Tests.Docker.xml'
 docker cp  ocaramba_selenium:/Ocaramba/TestResults/Ocaramba.Tests.Docker.xml .
 echo 'Uploading Ocaramba.Tests.Docker.xml'
