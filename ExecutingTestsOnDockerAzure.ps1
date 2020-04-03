49c50cc5fec24731170a3f7de19a1d5e344897a7
Jakub Raczek <jraczek@objectivity.co.uk>
2020-04-03 17:37:26 +0200

    Fixed ExecutingTestsOnDockerAzure.ps1

--- ExecutingTestsOnDockerAzure.ps1
+++ ExecutingTestsOnDockerAzure.ps1
@@ -1,16 +1,14 @@
 echo '********************************************Executing tests********************************************'
-        
-echo '********************************************netcoreapp3 tests********************************************'
+docker ps        
+echo 'build Ocaramba.sln'
+docker exec ocaramba_selenium dotnet build Ocaramba.sln
+echo 'execute Ocaramba.sln tests'
+docker exec ocaramba_selenium dotnet vstest Ocaramba.Tests.NUnit/bin/Debug/netcoreapp3.1/Ocaramba.Tests.NUnit.dll  /TestCaseFilter:"(TestCategory!=NotImplementedInCoreOrUploadDownload)" --logger:"trx;LogFileName=Ocaramba.Tests.Docker.xml"
+echo 'Downloading Ocaramba.Tests.Docker.xml'
+docker cp  ocaramba_selenium:/Ocaramba/TestResults/Ocaramba.Tests.Docker.xml .
+echo 'Uploading Ocaramba.Tests.Docker.xml'
 
-dotnet vstest .\Ocaramba.Tests.Angular\bin\Release\netcoreapp3.1\Ocaramba.Tests.Angular.dll `
-	          .\Ocaramba.Tests.NUnit\bin\Release\netcoreapp3.1\Ocaramba.Tests.NUnit.dll `
-			  .\Ocaramba.Tests.Features\bin\Release\netcoreapp3.1\Ocaramba.Tests.Features.dll `
-			  .\Ocaramba.Tests.Xunit\bin\Release\netcoreapp3.1\Ocaramba.Tests.Xunit.dll `
-			  .\Ocaramba.Tests.MsTest\bin\Release\netcoreapp3.1\Ocaramba.Tests.MsTest.dll `
-	          .\Ocaramba.UnitTests\bin\Release\netcoreapp3.1\Ocaramba.UnitTests.dll `
-			  /TestCaseFilter:"(TestCategory!=TakingScreehShots)" `
-	          --logger:"trx;LogFileName=Ocaramba.Tests.netcoreapp.xml"
-
+docker rm ocaramba_selenium --force
 
 if($lastexitcode -ne 0)
  {
--- ExecutingTestsOnWindowsAzure.ps1
+++ ExecutingTestsOnWindowsAzure.ps1
@@ -1,16 +1,19 @@
 echo '********************************************Executing tests********************************************'
+        
+echo '********************************************netcoreapp3 tests********************************************'
 
-docker info
+dotnet vstest .\Ocaramba.Tests.Angular\bin\Release\netcoreapp3.1\Ocaramba.Tests.Angular.dll `
+	          .\Ocaramba.Tests.NUnit\bin\Release\netcoreapp3.1\Ocaramba.Tests.NUnit.dll `
+			  .\Ocaramba.Tests.Features\bin\Release\netcoreapp3.1\Ocaramba.Tests.Features.dll `
+			  .\Ocaramba.Tests.Xunit\bin\Release\netcoreapp3.1\Ocaramba.Tests.Xunit.dll `
+			  .\Ocaramba.Tests.MsTest\bin\Release\netcoreapp3.1\Ocaramba.Tests.MsTest.dll `
+	          .\Ocaramba.UnitTests\bin\Release\netcoreapp3.1\Ocaramba.UnitTests.dll `
+			  /TestCaseFilter:"(TestCategory!=TakingScreehShots)" `
+	          --logger:"trx;LogFileName=Ocaramba.Tests.netcoreapp.xml"
 
-docker ps -a
-docker exec ocaramba_selenium sed -i '/Features/,+1 d' Ocaramba.sln
-docker exec ocaramba_selenium sed -i '/Documentation/,+5 d' Ocaramba.sln
-docker exec ocaramba_selenium dotnet build Ocaramba.sln
-docker exec ocaramba_selenium dotnet vstest Ocaramba.Tests.NUnit/bin/Debug/netcoreapp3.1/Ocaramba.Tests.NUnit.dll  /TestCaseFilter:"(TestCategory!=NotImplementedInCoreOrUploadDownload)" --logger:"trx;LogFileName=Ocaramba.Tests.Docker.xml"
+echo '********************************************net472 tests********************************************'
 
-docker cp  ocaramba_selenium:/Ocaramba/TestResults/Ocaramba.Tests.Docker.xml .
-
-
+$vstest = (Resolve-Path "D:\a\_temp\VsTest\Microsoft.TestPlatform*\tools\net*\Common*\IDE\Extensions\TestPlatform\vstest.console.exe").ToString()
 
 if($lastexitcode -ne 0)
  {
