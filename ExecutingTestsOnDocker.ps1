03d721543f31b449cbc16926a66464677f76c580
Jakub Raczek <jraczek@objectivity.co.uk>
2020-04-06 07:41:22 +0200

    Added ExecutingTestsOnLinuxAzure.ps1

--- ExecutingTestsOnDocker.ps1
+++ ExecutingTestsOnDocker.ps1
@@ -1,18 +1,13 @@
 echo '********************************************Executing tests on Docker********************************************'
 
+if($IsWindows){
 docker-switch-linux
-         
+         }
 docker info
          
-docker pull ocaramba/selenium
+#docker-compose up id --build
          
-docker build -t ocaramba/selenium -f DockerfileBuild .
-         
-docker run --rm -dit --name ocaramba_selenium ocaramba/selenium
 docker ps -a
-docker exec ocaramba_selenium sed -i '/Features/,+1 d' Ocaramba.sln
-docker exec ocaramba_selenium sed -i '/Documentation/,+5 d' Ocaramba.sln
-docker exec ocaramba_selenium dotnet build Ocaramba.sln
-docker exec ocaramba_selenium dotnet vstest Ocaramba.Tests.NUnit/bin/Debug/netcoreapp3.1/Ocaramba.Tests.NUnit.dll  /TestCaseFilter:"(TestCategory!=NotImplementedInCoreOrUploadDownload)" --logger:"trx;LogFileName=Ocaramba.Tests.Docker.xml"
-
+docker exec ocaramba_selenium bash -c "sed -i '/Features/,+1 d' Ocaramba.sln ; sed -i '/Documentation/,+5 d' Ocaramba.sln ; dotnet build Ocaramba.sln"
+docker exec ocaramba_selenium bash -c 'dotnet vstest Ocaramba.Tests.NUnit/bin/Debug/netcoreapp3.1/Ocaramba.Tests.NUnit.dll  /TestCaseFilter:\"(TestCategory!=NotImplementedInCoreOrUploadDownload)\" --logger:\"trx;LogFileName=Ocaramba.Tests.Docker.xml\"' 
 docker cp  ocaramba_selenium:/Ocaramba/TestResults/Ocaramba.Tests.Docker.xml .
---
+++ ExecutingTestsOnLinuxAzure.ps1
@@ -0,0 +1,18 @@
+echo '********************************************Executing tests********************************************'
+        
+echo '********************************************netcoreapp3 tests********************************************'
+
+dotnet vstest .\Ocaramba.Tests.Angular\bin\Release\netcoreapp3.1\Ocaramba.Tests.Angular.dll `
+	          .\Ocaramba.Tests.NUnit\bin\Release\netcoreapp3.1\Ocaramba.Tests.NUnit.dll `
+			  .\Ocaramba.Tests.Xunit\bin\Release\netcoreapp3.1\Ocaramba.Tests.Xunit.dll `
+			  .\Ocaramba.Tests.MsTest\bin\Release\netcoreapp3.1\Ocaramba.Tests.MsTest.dll `
+	          .\Ocaramba.UnitTests\bin\Release\netcoreapp3.1\Ocaramba.UnitTests.dll `
+			  /TestCaseFilter:"(TestCategory!=TakingScreehShots)" `
+	          --logger:"trx;LogFileName=Ocaramba.Tests.netcoreapp.xml"
+
+if($lastexitcode -ne 0)
+ {
+  echo 'lastexitcode' $lastexitcode
+ }
+ 
+exit 0
