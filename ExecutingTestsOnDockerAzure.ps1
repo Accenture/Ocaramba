8ee7daff56399584c8f8f0278842c67ab0e9a28d
Jakub Raczek <jraczek@objectivity.co.uk>
2020-04-06 07:56:36 +0200

    Updated azure-pipelines.yml

--- ExecutingTestsOnDockerAzure.ps1
+++ ExecutingTestsOnDockerAzure.ps1
@@ -1,14 +1,10 @@
-echo '********************************************Executing tests********************************************'
-docker ps        
-docker exec ocaramba_selenium sed -i '/Features/,+1 d' /Ocaramba/Ocaramba.sln
-docker exec ocaramba_selenium sed -i '/Documentation/,+5 d' /Ocaramba/Ocaramba.sln
-echo 'build Ocaramba.sln'
-docker exec ocaramba_selenium dotnet build /Ocaramba/Ocaramba.sln
-echo 'execute Ocaramba.sln tests'
-docker exec ocaramba_selenium dotnet vstest /Ocaramba/Ocaramba.Tests.NUnit/bin/Debug/netcoreapp3.1/Ocaramba.Tests.NUnit.dll  /TestCaseFilter:"(TestCategory!=NotImplementedInCoreOrUploadDownload)" --logger:"trx;LogFileName=Ocaramba.Tests.Docker.xml"
-echo 'Downloading Ocaramba.Tests.Docker.xml'
+docker info
+         
+docker ps -a
+docker exec ocaramba_selenium bash -c "sed -i '/Features/,+1 d' Ocaramba.sln ; sed -i '/Documentation/,+5 d' Ocaramba.sln ; dotnet build Ocaramba.sln"
+docker exec ocaramba_selenium bash -c 'dotnet vstest Ocaramba.Tests.NUnit/bin/Debug/netcoreapp3.1/Ocaramba.Tests.NUnit.dll  /TestCaseFilter:\"(TestCategory!=NotImplementedInCoreOrUploadDownload)\" --logger:\"trx;LogFileName=Ocaramba.Tests.Docker.xml\"' 
 docker cp  ocaramba_selenium:/Ocaramba/TestResults/Ocaramba.Tests.Docker.xml .
-echo 'Uploading Ocaramba.Tests.Docker.xml'
+
 
 docker rm ocaramba_selenium --force
 
