cb92d2f09838053e88c12404862f0af319a1e405
Jakub Raczek <jraczek@objectivity.co.uk>
2020-04-08 22:11:30 +0200

    Disabled all test except Nunit for linux

--- ExecutingTestsOnLinuxAzure.ps1
+++ ExecutingTestsOnLinuxAzure.ps1
@@ -2,11 +2,7 @@ echo '********************************************Executing tests***************
         
 echo '********************************************netcoreapp3 tests********************************************'
 
-dotnet vstest ./Ocaramba.Tests.Angular/bin/Release/netcoreapp3.1/Ocaramba.Tests.Angular.dll `
-	          ./Ocaramba.Tests.NUnit/bin/Release/netcoreapp3.1/Ocaramba.Tests.NUnit.dll `
-			  ./Ocaramba.Tests.Xunit/bin/Release/netcoreapp3.1/Ocaramba.Tests.Xunit.dll `
-			  ./Ocaramba.Tests.MsTest/bin/Release/netcoreapp3.1/Ocaramba.Tests.MsTest.dll `
-	          ./Ocaramba.UnitTests/bin/Release/netcoreapp3.1/Ocaramba.UnitTests.dll `
+dotnet vstest ./Ocaramba.Tests.NUnit/bin/Release/netcoreapp3.1/Ocaramba.Tests.NUnit.dll `
 			  /TestCaseFilter:"(TestCategory!=TakingScreehShots)" `
 	          --logger:"trx;LogFileName=Ocaramba.Tests.netcoreapp.xml"
 
