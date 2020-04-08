a617bf1e5d50589794d751103bba9650b110002e
Jakub Raczek <jraczek@objectivity.co.uk>
2020-04-08 21:19:55 +0200

    updated paths for docker

--- ExecutingTestsOnWindowsAzure.ps1
+++ ExecutingTestsOnWindowsAzure.ps1
@@ -2,8 +2,13 @@ echo '********************************************Executing tests***************
         
 echo '********************************************netcoreapp3 tests********************************************'
 
-dotnet vstest .\Ocaramba.Tests.NUnit\bin\Release\netcoreapp3.1\Ocaramba.Tests.NUnit.dll `
-			  /TestCaseFilter:"(TestCategory=NotImplementedInCoreOrUploadDownload)" `
+dotnet vstest .\Ocaramba.Tests.Angular\bin\Release\netcoreapp3.1\Ocaramba.Tests.Angular.dll `
+	          .\Ocaramba.Tests.NUnit\bin\Release\netcoreapp3.1\Ocaramba.Tests.NUnit.dll `
+			  .\Ocaramba.Tests.Features\bin\Release\netcoreapp3.1\Ocaramba.Tests.Features.dll `
+			  .\Ocaramba.Tests.Xunit\bin\Release\netcoreapp3.1\Ocaramba.Tests.Xunit.dll `
+			  .\Ocaramba.Tests.MsTest\bin\Release\netcoreapp3.1\Ocaramba.Tests.MsTest.dll `
+	          .\Ocaramba.UnitTests\bin\Release\netcoreapp3.1\Ocaramba.UnitTests.dll `
+			  /TestCaseFilter:"(TestCategory!=TakingScreehShots)" `
 	          --logger:"trx;LogFileName=Ocaramba.Tests.netcoreapp.xml"
 
 echo '********************************************net472 tests********************************************'
