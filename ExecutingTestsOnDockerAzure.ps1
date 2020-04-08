e077d21ae15cab88c75967b02a9f86ee729da4ac
Jakub Raczek <jraczek@objectivity.co.uk>
2020-04-08 12:47:28 +0200

    Fixed test execution on Linux

--- ExecutingTestsOnDockerAzure.ps1
+++ ExecutingTestsOnDockerAzure.ps1
@@ -1,8 +1,8 @@
 docker info
          
 docker ps -a
-docker exec ocaramba_selenium bash -c "sed -i '/Features/,+1 d' Ocaramba.sln ; sed -i '/Documentation/,+5 d' Ocaramba.sln ; dotnet build Ocaramba.sln"
-docker exec ocaramba_selenium bash -c 'dotnet vstest Ocaramba.Tests.NUnit/bin/Debug/netcoreapp3.1/Ocaramba.Tests.NUnit.dll  /TestCaseFilter:\"(TestCategory!=NotImplementedInCoreOrUploadDownload)\" --logger:\"trx;LogFileName=Ocaramba.Tests.Docker.xml\"' 
+docker exec ocaramba_selenium bash -c "ls ; sed -i '/Features/,+1 d' /Ocaramba/Ocaramba.sln ; sed -i '/Documentation/,+5 d' /Ocaramba/Ocaramba.sln ; dotnet build /Ocaramba/Ocaramba.sln"
+docker exec ocaramba_selenium bash -c 'dotnet vstest /Ocaramba/Ocaramba.Tests.NUnit/bin/Debug/netcoreapp3.1/Ocaramba.Tests.NUnit.dll  /TestCaseFilter:\"(TestCategory!=NotImplementedInCoreOrUploadDownload)\" --logger:\"trx;LogFileName=Ocaramba.Tests.Docker.xml\"' 
 docker cp  ocaramba_selenium:/Ocaramba/TestResults/Ocaramba.Tests.Docker.xml .
 
 
