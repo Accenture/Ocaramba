03cf630e8c636c71d831027abce93d473dc59aee
Jakub Raczek <jraczek@objectivity.co.uk>
2020-10-08 11:46:46 +0200

    Update ExecutingTestsOnLinux.ps1

--- ExecutingTestsOnLinux.ps1
+++ ExecutingTestsOnLinux.ps1
@@ -1,3 +1,8 @@
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+sudo apt-get update
+sudo apt-get install google-chrome-stable
+
 echo '********************************************Executing tests********************************************'
 
 echo '********************************************NUnit tests********************************************'   
