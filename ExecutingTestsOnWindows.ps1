5d86b96f3ec15bc07b967f804ed24de06aba9109
Jakub Raczek <jraczek@objectivity.co.uk>
2020-12-14 08:23:15 +0100

    Update ExecutingTestsOnWindows.ps1

--- ExecutingTestsOnWindows.ps1
+++ ExecutingTestsOnWindows.ps1
@@ -7,7 +7,7 @@ Sleep 1
 $wshell.SendKeys('chrome://settings/help')
 Sleep .5
 $wshell.SendKeys("{ENTER}")
-Sleep 180 # sleep to allow updates to be processed 
+Sleep 240 # sleep to allow updates to be processed 
 # change sleep values if computer is slow.
 $wshell.AppActivate('Chrome') # make sure chrome is the active window
 Sleep 1
