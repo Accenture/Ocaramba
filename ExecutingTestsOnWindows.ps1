72a74b9f0fd55af15d3907cf386967a6e5529d39
Jakub Raczek <jraczek@objectivity.co.uk>
2020-12-14 09:17:13 +0100

    Update ExecutingTestsOnWindows.ps1

--- ExecutingTestsOnWindows.ps1
+++ ExecutingTestsOnWindows.ps1
@@ -1,18 +1,3 @@
-echo '********************************************Update chrome********************************************'
-start chrome
-Sleep 5
-$wshell = New-Object -ComObject wscript.shell; # shell for sending keys
-$wshell.AppActivate('Chrome') # make sure chrome is the active window
-Sleep 5
-$wshell.SendKeys('chrome://settings/help')
-Sleep 5
-$wshell.SendKeys("{ENTER}")
-Sleep 180 # sleep to allow updates to be processed 
-# change sleep values if computer is slow.
-$wshell.AppActivate('Chrome') # make sure chrome is the active window
-Sleep 5
-$wshell.SendKeys("%{F4}") # ALT F4
-
 echo '********************************************Executing tests********************************************'
         
 echo '********************************************NUnit tests********************************************'
