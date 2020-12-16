6dffa3636fe37f255813c5d86c2353b35e0b24e5
Jakub Raczek <jraczek@objectivity.co.uk>
2020-12-16 21:54:28 +0100

    Update ExecutingTestsOnWindowsAzure3.ps1

--- ExecutingTestsOnWindowsAzure3.ps1
+++ ExecutingTestsOnWindowsAzure3.ps1
@@ -3,9 +3,9 @@ echo '********************************************Executing tests***************
 echo '********************************************net472 tests*********************************************'
 $vstest = (Resolve-Path "D:\a\_temp\VsTest\Microsoft.TestPlatform*\tools\net*\Common*\IDE\Extensions\TestPlatform\vstest.console.exe").ToString()
 
-& $vstest .\Ocaramba.Tests.Angular\bin\Debug\net472\Ocaramba.Tests.Angular.dll `
-			.\Ocaramba.Tests.NUnit\bin\Debug\net472\Ocaramba.Tests.NUnit.dll `
-			.\Ocaramba.UnitTests\bin\Debug\net472\Ocaramba.UnitTests.dll `
+& $vstest .\Ocaramba.Tests.Angular\bin\Release\net472\Ocaramba.Tests.Angular.dll `
+			.\Ocaramba.Tests.NUnit\bin\Release\net472\Ocaramba.Tests.NUnit.dll `
+			.\Ocaramba.UnitTests\bin\Release\net472\Ocaramba.UnitTests.dll 
 			--logger:"trx;LogFileName=Ocaramba.Tests.net4.xml"
 
 
