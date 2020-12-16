144fcc3503dccf76d5dbf30f32e129d809592ace
Jakub Raczek <jraczek@objectivity.co.uk>
2020-12-16 14:10:38 +0100

    Divide test execution between jobs

--- ExecutingTestsOnWindowsAzure1.ps1
+++ ExecutingTestsOnWindowsAzure1.ps1
@@ -4,10 +4,6 @@ echo '********************************************netcoreapp3 tests*************
 
 dotnet vstest .\Ocaramba.Tests.Angular\bin\Release\netcoreapp3.1\Ocaramba.Tests.Angular.dll `
 	          .\Ocaramba.Tests.NUnit\bin\Release\netcoreapp3.1\Ocaramba.Tests.NUnit.dll `
-			  .\Ocaramba.Tests.Features\bin\Release\netcoreapp3.1\Ocaramba.Tests.Features.dll `
-			  .\Ocaramba.Tests.Xunit\bin\Release\netcoreapp3.1\Ocaramba.Tests.Xunit.dll `
-			  .\Ocaramba.Tests.MsTest\bin\Release\netcoreapp3.1\Ocaramba.Tests.MsTest.dll `
-	          .\Ocaramba.UnitTests\bin\Release\netcoreapp3.1\Ocaramba.UnitTests.dll `
 			  /TestCaseFilter:"(TestCategory!=TakingScreehShots)" `
 	          --logger:"trx;LogFileName=Ocaramba.Tests.netcoreapp.xml"
 
