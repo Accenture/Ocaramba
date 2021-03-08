2e13261447d8dfff78e064e464a23fbdd875b254
Jakub Raczek <jraczek@objectivity.co.uk>
2021-03-08 11:27:09 +0100

    Update Chrome on Azure to the newest

--- ExecutingTestsOnWindowsAzure3.ps1
+++ ExecutingTestsOnWindowsAzure3.ps1
@@ -1,3 +1,11 @@
+echo '********************************************Update Chrome********************************************'
+$LocalTempDir = $env:TEMP; $ChromeInstaller = "ChromeInstaller.exe"; 
+(new-object    System.Net.WebClient).DownloadFile('http://dl.google.com/chrome/install/375.126/chrome_installer.exe', "$LocalTempDir\$ChromeInstaller");
+& "$LocalTempDir\$ChromeInstaller" /silent /install; $Process2Monitor =  "ChromeInstaller"; 
+Do { $ProcessesFound = Get-Process | ?{$Process2Monitor -contains $_.Name} | Select-Object -ExpandProperty Name;
+If ($ProcessesFound) { "Still running: $($ProcessesFound -join ', ')" | Write-Host; Start-Sleep -Seconds 2 } 
+else { rm "$LocalTempDir\$ChromeInstaller" -ErrorAction SilentlyContinue -Verbose } } Until (!$ProcessesFound)
+
 echo '********************************************Executing tests********************************************'
         
 echo '********************************************net472 tests*********************************************'
