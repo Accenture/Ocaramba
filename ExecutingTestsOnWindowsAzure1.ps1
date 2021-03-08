068cb94c75da8028e8ddc6b54557876c64b315ac
Jakub Raczek <jraczek@objectivity.co.uk>
2021-03-08 12:34:46 +0100

    Update ExecutingTestsOnWindowsAzure1.ps1

--- ExecutingTestsOnWindowsAzure1.ps1
+++ ExecutingTestsOnWindowsAzure1.ps1
@@ -1,10 +1,16 @@
 echo '********************************************Update Chrome********************************************'
-$LocalTempDir = $env:TEMP; $ChromeInstaller = "ChromeInstaller.exe"; 
-(new-object    System.Net.WebClient).DownloadFile('http://dl.google.com/chrome/install/375.126/chrome_installer.exe', "$LocalTempDir\$ChromeInstaller");
-& "$LocalTempDir\$ChromeInstaller" /silent /install; $Process2Monitor =  "ChromeInstaller"; 
-Do { $ProcessesFound = Get-Process | ?{$Process2Monitor -contains $_.Name} | Select-Object -ExpandProperty Name;
-If ($ProcessesFound) { "Still running: $($ProcessesFound -join ', ')" | Write-Host; Start-Sleep -Seconds 2 } 
-else { rm "$LocalTempDir\$ChromeInstaller" -ErrorAction SilentlyContinue -Verbose } } Until (!$ProcessesFound)
+$LocalTempDir = ".\"; $ChromeInstaller = "ChromeInstaller.msi"; 
+(new-object    System.Net.WebClient).DownloadFile('https://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7B16597FD3-D497-42D5-1E46-04BAE17B6B4E%7D%26lang%3Den%26browser%3D5%26usagestats%3D0%26appname%3DGoogle%2520Chrome%26needsadmin%3Dprefers%26ap%3Dx64-stable-statsdef_1%26installdataindex%3Dempty/chrome/install/ChromeStandaloneSetup64.exe', "$LocalTempDir\$ChromeInstaller");
+
+$list = 
+@(
+    "/I `"$LocalTempDir\$ChromeInstaller`"",                     # Install this MSI
+    "/QN",                             # Quietly, without a UI
+    "/L*V `".\ChromeIns.log`""     # Verbose output to this log
+)
+$p = Start-Process -FilePath "msiexec" -ArgumentList $list -Wait 
+
+rm "$LocalTempDir\$ChromeInstaller" -ErrorAction SilentlyContinue -Verbose
 
 echo '********************************************Executing tests********************************************'
         
