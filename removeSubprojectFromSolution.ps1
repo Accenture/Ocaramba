3935925a19bde351bae8e34bafc96348e20a03e9
Jakub Raczek <jraczek@objectivity.co.uk>
2021-06-24 09:38:17 +0200

    Remove Documentation from build in CI

--- removeSubprojectFromSolution.ps1
+++ removeSubprojectFromSolution.ps1
@@ -1,6 +1,6 @@
 echo "removing Documentation subprojects from .\Ocaramba.sln"
 echo $isWindows
-if (-Not $isWindows) {    
+#if (-Not $isWindows) {    
 echo "removing Documentation subprojects from .\Ocaramba.sln"
 (Get-Content '.\Ocaramba.sln' -raw) -replace [regex]('.*?' + 'Documentation' + ('.*?\n' * (5 + 1))) | set-content  '.\Ocaramba.sln'
-	}
+#	}
