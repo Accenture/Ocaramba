4d7ead9a2f58bfcb652c7498986ddf74d0dbfbbb
Jakub Raczek <jraczek@objectivity.co.uk>
2021-07-21 09:19:19 +0200

    Run test on EdgeChrominum

--- ExecutingTestsOnWindowsAzure2.ps1
+++ ExecutingTestsOnWindowsAzure2.ps1
@@ -18,9 +18,38 @@ dotnet vstest .\Ocaramba.Tests.Features\bin\Release\netcoreapp3.1\Ocaramba.Tests
 			  /TestCaseFilter:"(TestCategory!=TakingScreehShots)" /Parallel `
 	          --logger:"trx;LogFileName=Ocaramba.Tests.netcoreapp.xml"
 
-echo '********************************************net472 tests********************************************'
 
-$vstest = (Resolve-Path "D:\a\_temp\VsTest\Microsoft.TestPlatform*\tools\net*\Common*\IDE\Extensions\TestPlatform\vstest.console.exe").ToString()
+echo '********************************************EdgeChrominum tests********************************************'
+
+$url = $env:edgeChromiumDriverUrl
+echo url: $url
+$outputZip = "edgedriver_win64.zip"	
+$output = $PSScriptRoot + "\Ocaramba.Tests.NUnit\bin\Release\netcoreapp3.1\$outputZip"
+$outputPath = $PSScriptRoot + "\Ocaramba.Tests.NUnit\bin\Release\netcoreapp3.1"
+echo output: $output
+echo outputPath: $outputPath
+
+
+echo "Downloading EdgeChrominum driver from: $($url) to $($output)"
+Invoke-WebRequest -Uri "$($url)" -Out "$($output)"  
+Expand-Archive -LiteralPath $outputZip -DestinationPath $outputPath -Force
+$driver=$outputPath+"msedgedriver.exe"
+
+echo driver: $driver
+
+.\scripts\set_AppConfig_for_tests.ps1 ".\Ocaramba.Tests.NUnit\bin\Release\netcoreapp3.1\" "appsettings.json" "appSettings" "browser|PathToEdgeChrominumDriverDirectory" "EdgeChromium|$driver" $true $true
+
+echo "Downloading edgeChromiumDriver from:" $env:edgeChromiumDriverUrl
+        
+$outputZip = $env:APPVEYOR_BUILD_FOLDER + "\Ocaramba.Tests.NUnit\bin\Debug\net45\edgedriver_win64.zip"	
+		
+(New-Object System.Net.WebClient).DownloadFile($env:edgeChromiumDriverUrl, $outputZip)
+
+Expand-Archive -LiteralPath $outputZip -DestinationPath $output  -Force
+
+dotnet vstest .\Ocaramba.Tests.NUnit\bin\Release\netcoreapp3.1\Ocaramba.Tests.NUnit.dll `
+			  /TestCaseFilter:"(TestCategory=BasicNUnit)" /Parallel `
+	          --logger:"trx;LogFileName=Ocaramba.Tests.EdgeChrominum.xml"
 
 if($lastexitcode -ne 0)
  {
