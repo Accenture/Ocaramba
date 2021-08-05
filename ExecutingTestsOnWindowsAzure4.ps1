dde42de0ed721919217f4fd9d83523b9f4281d2b
Jakub Raczek <jraczek@objectivity.co.uk>
2021-08-05 14:25:44 +0200

    Removed test on testingbot.com

--- ExecutingTestsOnWindowsAzure4.ps1
+++ ExecutingTestsOnWindowsAzure4.ps1
@@ -29,20 +29,6 @@ if($lastexitcode -ne 0)
   echo 'lastexitcode' $lastexitcode
  } 
  
-echo '********************************************testingbot.com tests********************************************'  
-    
-.\scripts\set_AppConfig_for_tests.ps1 ".\Ocaramba.Tests.CloudProviderCrossBrowser\bin\Release\net472" "Ocaramba.Tests.CloudProviderCrossBrowser.dll.config" "//appSettings" "RemoteWebDriverHub" "https://$($env:MAPPED_ENV_TESTINGBOTKEY):$($env:MAPPED_ENV_TESTINGBOTSECRET)@hub.testingbot.com/wd/hub/"
-    
-.\scripts\set_AppConfig_for_tests.ps1 ".\Ocaramba.Tests.CloudProviderCrossBrowser\bin\Release\net472" "Ocaramba.Tests.CloudProviderCrossBrowser.dll.config" "//DriverCapabilities" "buildName" "$Ocaramba.Tests.TestingBotCrossBrowser$($env:BuildVersion)" $true
-   
-& $vstest  .\Ocaramba.Tests.CloudProviderCrossBrowser\bin\Release\net472\Ocaramba.Tests.CloudProviderCrossBrowser.dll /TestCaseFilter:"(FullyQualifiedName!~Iphone)&(FullyQualifiedName!~Android)&(FullyQualifiedName!~Samsung)" `
-			--logger:"trx;LogFileName=Ocaramba.Tests.testingbot.xml"
-			
-if($lastexitcode -ne 0)
- {
-  echo 'lastexitcode' $lastexitcode
- }
- 
 echo '********************************************saucelabs tests********************************************'
     
 .\scripts\set_AppConfig_for_tests.ps1 ".\Ocaramba.Tests.CloudProviderCrossBrowser\bin\Release\net472" "Ocaramba.Tests.CloudProviderCrossBrowser.dll.config" "//appSettings" "RemoteWebDriverHub" "https://$($env:MAPPED_ENV_SAUCELABSUSERNAME):$($env:MAPPED_ENV_SAUCELABSACCESSKEY)@ondemand.us-west-1.saucelabs.com:443/wd/hub"
