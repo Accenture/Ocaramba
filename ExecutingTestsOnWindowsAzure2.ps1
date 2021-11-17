606fad3f6ca0ddaba91a89276b5bfeb17ac71df0
Jakub Raczek <jraczek@objectivity.co.uk>
2021-11-17 09:36:20 +0100

    fix build issue

--- ExecutingTestsOnWindowsAzure2.ps1
+++ ExecutingTestsOnWindowsAzure2.ps1
@@ -14,11 +14,6 @@ echo '********************************************EdgeChrominum tests***********
 
 .\scripts\set_AppConfig_for_tests.ps1 ".\Ocaramba.Tests.NUnit\bin\Release\netcoreapp3.1" "appsettings.json" "appSettings" "browser|PathToEdgeChromiumDriverDirectory" "EdgeChromium|$($env:EDGEWEBDRIVER)" $true $true
         
-$outputZip = $PSScriptRoot + "\Ocaramba.Tests.NUnit\bin\Release\netcoreapp3.1\edgedriver_win64.zip"	
-echo outputZip: $outputZip
-Invoke-WebRequest -Uri "$($url)" -Out "$($output)"  		
-
-Expand-Archive -LiteralPath $outputZip -DestinationPath $outputPath  -Force
 
 dotnet vstest .\Ocaramba.Tests.NUnit\bin\Release\netcoreapp3.1\Ocaramba.Tests.NUnit.dll `
 			  /TestCaseFilter:"(TestCategory=Grid)" /Parallel `
