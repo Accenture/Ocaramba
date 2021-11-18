a5921a414db2e7f22dc78cc23738d933394b171b
Jakub Raczek <jraczek@objectivity.co.uk>
2021-11-18 19:05:20 +0100

    Added test category NotImplementedInCoreOrUploadDownload

--- ExecutingTestsOnLinuxAzure.ps1
+++ ExecutingTestsOnLinuxAzure.ps1
@@ -8,7 +8,7 @@ echo $Env:ASPNETCORE_ENVIRONMENT
 .\scripts\set_AppConfig_for_tests.ps1 ".\Ocaramba.Tests.NUnit\bin\Release\netcoreapp3.1" "appsettings.json" "appSettings" "browser|PathToChromeDriverDirectory" "Chrome|$($env:CHROMEWEBDRIVER)" -logValues -json
 
 dotnet vstest ./Ocaramba.Tests.NUnit/bin/Release/netcoreapp3.1/Ocaramba.Tests.NUnit.dll `
-			  /TestCaseFilter:"(TestCategory!=TakingScreehShots)" /Parallel `
+			  /TestCaseFilter:"(TestCategory!=NotImplementedInCoreOrUploadDownload)" /Parallel `
 	          --logger:"trx;LogFileName=Ocaramba.Tests.netcoreapp.xml"
 
 if($lastexitcode -ne 0)
