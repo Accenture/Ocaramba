bfe556db58cc182f677764af7635a2faddf59946
Jakub Raczek <jraczek@objectivity.co.uk>
2021-11-22 13:03:08 +0100

    Update ExecutingTestsOnLinuxAzure.ps1

--- ExecutingTestsOnLinuxAzure.ps1
+++ ExecutingTestsOnLinuxAzure.ps1
@@ -5,7 +5,7 @@ echo '********************************************netcoreapp3 tests*************
 $Env:ASPNETCORE_ENVIRONMENT="Linux"
 
 echo $Env:ASPNETCORE_ENVIRONMENT
-.\scripts\set_AppConfig_for_tests.ps1 ".\Ocaramba.Tests.NUnit\bin\Release\netcoreapp3.1" "appsettings.json" "appSettings" "browser|PathToChromeDriverDirectory" "Chrome|$($env:CHROMEWEBDRIVER)" -logValues -json
+.\scripts\set_AppConfig_for_tests.ps1 ".\Ocaramba.Tests.NUnit\bin\Release\netcoreapp3.1" "appsettings.Linux.json" "appSettings" "browser|PathToChromeDriverDirectory" "Chrome|$($env:CHROMEWEBDRIVER)" -logValues -json
 
 dotnet vstest ./Ocaramba.Tests.NUnit/bin/Release/netcoreapp3.1/Ocaramba.Tests.NUnit.dll `
 			  /TestCaseFilter:"(TestCategory!=NotImplementedInCoreOrUploadDownload)" /Parallel `
