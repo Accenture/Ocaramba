79d6b7e4f1b6f550404513ce6bef04fee8dd902b
Maciej Antosiak <mantosiak@objectivity.co.uk>
2024-03-07 11:06:45 +0100

    fixing windows framework

---
+++ ExecutingTestsOnLinuxBrowserStackGithubActions.ps1
@@ -0,0 +1,28 @@
+echo '********************************************CloudProviderCrossBrowser tests********************************************'
+        
+echo '********************************************BrowserStack tests********************************************'
+$Env:ASPNETCORE_ENVIRONMENT="Linux"
+
+echo $Env:ASPNETCORE_ENVIRONMENT
+echo $env:MAPPED_ENV_BROWSERSTACKUSER
+echo $env:MAPPED_ENV_BROWSERSTACKKEY
+echo $Env:MAPPED_ENV_BROWSERSTACKUSER
+echo $Env:MAPPED_ENV_BROWSERSTACKKEY
+echo $Env:MAPPED_ENV_TESTINGBOTKEY
+echo $Env:MAPPED_ENV_TESTINGBOTSECRET
+echo $Env:MAPPED_ENV_SAUCELABSACCESSKEY
+echo $Env:MAPPED_ENV_SAUCELABSUSERNAME
+
+.\scripts\set_AppConfig_for_tests.ps1 ".\Ocaramba.Tests.CloudProviderCrossBrowser\bin\Release\net6.0" "appsettings.Linux.json" "appSettings" "browser|PathToChromeDriverDirectory" "Chrome|$($env:CHROMEWEBDRIVER)" -logValues -json
+
+.\scripts\set_AppConfig_for_tests.ps1 ".\Ocaramba.Tests.CloudProviderCrossBrowser\bin\Release\net6.0" "appsettings.Linux.json" "appSettings" "RemoteWebDriverHub" "https://$($env:MAPPED_ENV_BROWSERSTACKUSER):$($env:MAPPED_ENV_BROWSERSTACKKEY)@hub-cloud.browserstack.com/wd/hub" -json
+        
+.\scripts\set_AppConfig_for_tests.ps1 ".\Ocaramba.Tests.CloudProviderCrossBrowser\bin\Release\net6.0" "appsettings.Linux.json" "DriverCapabilities" "buildName" "Ocaramba.Tests.BrowserStackCrossBrowser$($env:BuildVersion)" -logValues -json
+
+dotnet vstest ./Ocaramba.Tests.CloudProviderCrossBrowser/bin/Release/net6.0/Ocaramba.Tests.CloudProviderCrossBrowser.dll /Logger:"trx;LogFileName=Ocaramba.Tests.BrowserStacknetcoreapp.xml"
+		  
+if($lastexitcode -ne 0)
+ {
+  echo 'lastexitcode' $lastexitcode
+ } 
+exit 0    
