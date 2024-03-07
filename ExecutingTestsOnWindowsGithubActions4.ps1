314c495fbd557607797bc02247bb9c43c1854dc8
Maciej Antosiak <mantosiak@objectivity.co.uk>
2024-03-07 12:46:59 +0100

    reverting changes in ps1 scripts, fixing linux browserstack

--- ExecutingTestsOnWindowsGithubActions4.ps1
+++ ExecutingTestsOnWindowsGithubActions4.ps1
@@ -1,12 +1,7 @@
 echo '********************************************CloudProviderCrossBrowser tests********************************************'
         
 echo '********************************************BrowserStack tests********************************************'
-echo $($env:MAPPED_ENV_BROWSERSTACKUSER)
-echo $($env:MAPPED_ENV_BROWSERSTACKKEY)
-echo $env:MAPPED_ENV_BROWSERSTACKUSER
-echo $env:MAPPED_ENV_BROWSERSTACKKEY
-
-
+  
 $vstest = (Resolve-Path "D:\a\_temp\VsTest\Microsoft.TestPlatform*\tools\net*\Common*\IDE\Extensions\TestPlatform\vstest.console.exe").ToString()
        
 .\scripts\set_AppConfig_for_tests.ps1 ".\Ocaramba.Tests.CloudProviderCrossBrowser\bin\Release\net472" "Ocaramba.Tests.CloudProviderCrossBrowser.dll.config" "//appSettings" "RemoteWebDriverHub" "https://$($env:MAPPED_ENV_BROWSERSTACKUSER):$($env:MAPPED_ENV_BROWSERSTACKKEY)@hub-cloud.browserstack.com/wd/hub"
