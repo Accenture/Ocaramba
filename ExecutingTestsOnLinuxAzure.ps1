6552a15eb8d15359ad66718efc04dc8f65f1a7d9
Maciej Antosiak <mantosiak@objectivity.co.uk>
2024-03-12 14:00:54 +0100

    rolling back linux azure changes

--- ExecutingTestsOnLinuxAzure.ps1
+++ ExecutingTestsOnLinuxAzure.ps1
@@ -5,9 +5,11 @@ echo '********************************************net6.0 tests******************
 $Env:ASPNETCORE_ENVIRONMENT="Linux"
 
 echo $Env:ASPNETCORE_ENVIRONMENT
-#.\scripts\set_AppConfig_for_tests.ps1 ".\Ocaramba.Tests.NUnit\bin\Release\net6.0" "appsettings.Linux.json" "appSettings" "browser|PathToChromeDriverDirectory" "Chrome|$($env:CHROMEWEBDRIVER)" -logValues -json
+.\scripts\set_AppConfig_for_tests.ps1 ".\Ocaramba.Tests.NUnit\bin\Release\net6.0" "appsettings.Linux.json" "appSettings" "browser|PathToChromeDriverDirectory" "Chrome|$($env:CHROMEWEBDRIVER)" -logValues -json
 
-dotnet vstest ./Ocaramba.Tests.NUnit/bin/Release/net6.0/Ocaramba.Tests.NUnit.dll /TestCaseFilter:"(TestCategory!=NotImplementedInCoreOrUploadDownload)" /Parallel /Logger:"trx;LogFileName=Ocaramba.Tests.netcoreapp.xml"
+dotnet vstest ./Ocaramba.Tests.NUnit/bin/Release/net6.0/Ocaramba.Tests.NUnit.dll `
+			  /TestCaseFilter:"(TestCategory!=NotImplementedInCoreOrUploadDownload)" /Parallel `
+	          --logger:"trx;LogFileName=Ocaramba.Tests.netcoreapp.xml"
 
 if($lastexitcode -ne 0)
  {
