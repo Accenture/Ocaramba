531970d2405db02d1cdb4f696052c0daad2a2f0b
Jakub Raczek <jraczek@objectivity.co.uk>
2024-11-27 15:30:16 +0100

    Update ExecutingTestsOnLinuxAzure.ps1

--- ExecutingTestsOnLinuxAzure.ps1
+++ ExecutingTestsOnLinuxAzure.ps1
@@ -5,11 +5,9 @@ echo '********************************************net8.0 tests******************
 $Env:ASPNETCORE_ENVIRONMENT="Linux"
 
 echo $Env:ASPNETCORE_ENVIRONMENT
-.\scripts\set_AppConfig_for_tests.ps1 ".\Ocaramba.Tests.NUnit\bin\Release\net8.0" "appsettings.Linux.json" "appSettings" "browser|PathToChromeDriverDirectory" "Chrome|$($env:CHROMEWEBDRIVER)" -logValues -json
+.\scripts\set_AppConfig_for_tests.ps1 ".\Ocaramba.Tests.NUnit\bin\Release\net8.0" "appsettings.Linux.json" "appSettings" "browser|PathToChromeDriverDirectory" "Chrome|/usr/local/bin" -logValues -json
 
-dotnet vstest ./Ocaramba.Tests.NUnit/bin/Release/net8.0/Ocaramba.Tests.NUnit.dll `
-			  /TestCaseFilter:"(TestCategory!=NotImplementedInCoreOrUploadDownload)" /Parallel `
-	          --logger:"trx;LogFileName=Ocaramba.Tests.netcoreapp.xml"
+dotnet vstest ./Ocaramba.Tests.NUnit/bin/Release/net8.0/Ocaramba.Tests.NUnit.dll /TestCaseFilter:"(TestCategory!=NotImplementedInCoreOrUploadDownload)" /Parallel --logger:"trx;LogFileName=Ocaramba.Tests.netcoreapp.xml"
 
 if($lastexitcode -ne 0)
  {
