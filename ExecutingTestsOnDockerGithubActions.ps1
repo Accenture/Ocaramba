e3738e9143734e496fae730446bafa766432b4d8
Jakub Raczek <jraczek@objectivity.co.uk>
2024-11-28 11:07:51 +0100

    Update ExecutingTestsOnDockerGithubActions.ps1

--- ExecutingTestsOnDockerGithubActions.ps1
+++ ExecutingTestsOnDockerGithubActions.ps1
@@ -4,7 +4,7 @@ docker ps -a
 docker exec ocaramba_selenium bash -c "ls ; sed -i '/Documentation/,+5 d' ./Ocaramba.sln ; dotnet build ./Ocaramba.sln"
 docker exec ocaramba_selenium bash -c 'pwsh ./scripts/set_AppConfig_for_tests.ps1 "./Ocaramba.Tests.NUnit/bin/Debug/net8.0" "appsettings.Linux.json" "appSettings" "browser|PathToChromeDriverDirectory" "Chrome|/usr/local/bin/" -logValues -json'
 docker exec ocaramba_selenium bash -c 'dotnet vstest ./Ocaramba.Tests.NUnit/bin/Debug/net8.0/Ocaramba.Tests.NUnit.dll /TestCaseFilter:"(TestCategory!=NotImplementedInCoreOrUploadDownload)" /Parallel --logger:trx;LogFileName=Ocaramba.Tests.Docker.xml' 
-
+docker exec ocaramba_selenium bash -c "ls"
 docker cp  ocaramba_selenium:/TestResults/Ocaramba.Tests.Docker.xml .
 
 docker rm ocaramba_selenium --force
