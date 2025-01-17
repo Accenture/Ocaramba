fc864b68bef34f4a46b2894a8bfee6fd851735ca
Jakub Raczek <jraczek@objectivity.co.uk>
2025-01-17 19:00:24 +0100

    Update ExecutingTestsOnWindowsGithubActions4.ps1

--- scripts/ExecutingTestsOnWindowsGithubActions4.ps1
+++ scripts/ExecutingTestsOnWindowsGithubActions4.ps1
@@ -1,7 +1,15 @@
 echo '********************************************CloudProviderCrossBrowser tests********************************************'
         
 echo '********************************************BrowserStack tests********************************************'
-       
+$filePath = ".\Ocaramba.Tests.CloudProviderCrossBrowser\bin\Release\net8.0\browserstack.yml"
+
+$fileContent = Get-Content -Path $filePath
+
+$fileContent = $fileContent -replace "BROWSERSTACKUSER", "`$($env:MAPPED_ENV_BROWSERSTACKUSER)"
+$fileContent = $fileContent -replace "BROWSERSTACKKEY", "`$($env:MAPPED_ENV_BROWSERSTACKKEY)"
+
+# Write the updated content back to the file
+Set-Content -Path $filePath -Value $fileContent       
 .\scripts\set_AppConfig_for_tests.ps1 ".\Ocaramba\Ocaramba.Tests.CloudProviderCrossBrowser\bin\Release\net8.0" "appsettings.json" "appSettings" "RemoteWebDriverHub" "https://$($env:MAPPED_ENV_BROWSERSTACKUSER):$($env:MAPPED_ENV_BROWSERSTACKKEY)@hub-cloud.browserstack.com/wd/hub" -json
 .\scripts\set_AppConfig_for_tests.ps1 ".\Ocaramba\Ocaramba.Tests.CloudProviderCrossBrowser\bin\Release\net8.0" "appsettings.json" "DriverCapabilities" "buildName" "Ocaramba.Tests.BrowserStackCrossBrowser$($env:BuildVersion)" -logValues -json
 
