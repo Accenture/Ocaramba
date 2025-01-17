642a4cebe30b5d4c95901ae5e91ff04eb663d41f
Jakub Raczek <jakub.raczek@accenture.com>
2025-01-17 14:54:21 +0100

    fix

--- scripts/ExecutingTestsOnLinuxAzure.ps1
+++ scripts/ExecutingTestsOnLinuxAzure.ps1
@@ -6,14 +6,7 @@ $Env:ASPNETCORE_ENVIRONMENT="Linux"
 
 echo $Env:ASPNETCORE_ENVIRONMENT
 
-$chromeDriver = Get-ChildItem -Path "/opt/hostedtoolcache/setup-chrome/chromedriver/" -Recurse -Filter "chromedriver" | Select-Object -First 1
-$chromeDriverPath = Split-Path -Path $chromeDriver.FullName
-
-$chrome = Get-ChildItem -Path "/opt/hostedtoolcache/setup-chrome/chromium/" -Recurse -Filter "chrome" | Select-Object -First 1
-$chromePath = Split-Path -Path $chrome.FullName
-
-
-.\scripts\set_AppConfig_for_tests.ps1 ".\Ocaramba.Tests.NUnit\bin\Release\net8.0" "appsettings.Linux.json" "appSettings" "browser|PathToChromeDriverDirectory|ChromeBrowserExecutableLocation" "Chrome|$chromeDriverPath|$chromePath" -logValues -json
+.\scripts\set_AppConfig_for_tests.ps1 ".\Ocaramba.Tests.NUnit\bin\Release\net8.0" "appsettings.Linux.json" "appSettings" "browser" "Chrome" -logValues -json
 
 dotnet vstest ./Ocaramba.Tests.NUnit/bin/Release/net8.0/Ocaramba.Tests.NUnit.dll /TestCaseFilter:"(TestCategory!=NotImplementedInCoreOrUploadDownload)" /Parallel --logger:"trx;LogFileName=Ocaramba.Tests.netcoreapp.xml"
 
