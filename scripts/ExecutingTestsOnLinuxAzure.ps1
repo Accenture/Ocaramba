3276d26868fb5bcbd66df0f7da390827da151d3c
Jakub Raczek <jakub.raczek@accenture.com>
2025-01-17 14:07:34 +0100

    fixed build issue

--- scripts/ExecutingTestsOnLinuxAzure.ps1
+++ scripts/ExecutingTestsOnLinuxAzure.ps1
@@ -5,7 +5,7 @@ echo '********************************************net8.0 tests******************
 $Env:ASPNETCORE_ENVIRONMENT="Linux"
 
 echo $Env:ASPNETCORE_ENVIRONMENT
-.\scripts\set_AppConfig_for_tests.ps1 ".\Ocaramba.Tests.NUnit\bin\Release\net8.0" "appsettings.Linux.json" "appSettings" "browser|PathToChromeDriverDirectory" "Chrome|/usr/local/bin" -logValues -json
+.\scripts\set_AppConfig_for_tests.ps1 ".\Ocaramba.Tests.NUnit\bin\Release\net8.0" "appsettings.Linux.json" "appSettings" "browser|PathToChromeDriverDirectory" "Chrome|/opt/hostedtoolcache/setup-chrome/chromedriver/1407831/x64" -logValues -json
 
 dotnet vstest ./Ocaramba.Tests.NUnit/bin/Release/net8.0/Ocaramba.Tests.NUnit.dll /TestCaseFilter:"(TestCategory!=NotImplementedInCoreOrUploadDownload)" /Parallel --logger:"trx;LogFileName=Ocaramba.Tests.netcoreapp.xml"
 
