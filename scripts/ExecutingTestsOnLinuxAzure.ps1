126a8c016b5372b09636106ffc415bf72b596a62
Jakub Raczek <jakub.raczek@accenture.com>
2025-01-17 14:36:30 +0100

    fixed path

--- scripts/ExecutingTestsOnLinuxAzure.ps1
+++ scripts/ExecutingTestsOnLinuxAzure.ps1
@@ -5,7 +5,15 @@ echo '********************************************net8.0 tests******************
 $Env:ASPNETCORE_ENVIRONMENT="Linux"
 
 echo $Env:ASPNETCORE_ENVIRONMENT
-.\scripts\set_AppConfig_for_tests.ps1 ".\Ocaramba.Tests.NUnit\bin\Release\net8.0" "appsettings.Linux.json" "appSettings" "browser|PathToChromeDriverDirectory|ChromeBrowserExecutableLocation" "Chrome|/opt/hostedtoolcache/setup-chrome/chromedriver/1407837/x64/|/opt/hostedtoolcache/setup-chrome/chromium/1407831/x64/" -logValues -json
+
+$chromeDriver = Get-ChildItem -Path "/opt/hostedtoolcache/setup-chrome/chromedriver/" -Recurse -Filter "chromedriver" | Select-Object -First 1
+$chromeDriverPath = Split-Path -Path $chromeDriver.FullName
+
+$chrome = Get-ChildItem -Path "/opt/hostedtoolcache/setup-chrome/chromium/" -Recurse -Filter "chromedriver" | Select-Object -First 1
+$chromePath = Split-Path -Path $chrome.FullName
+
+
+.\scripts\set_AppConfig_for_tests.ps1 ".\Ocaramba.Tests.NUnit\bin\Release\net8.0" "appsettings.Linux.json" "appSettings" "browser|PathToChromeDriverDirectory|ChromeBrowserExecutableLocation" "Chrome|$chromeDriverPath|$chromePath" -logValues -json
 
 dotnet vstest ./Ocaramba.Tests.NUnit/bin/Release/net8.0/Ocaramba.Tests.NUnit.dll /TestCaseFilter:"(TestCategory!=NotImplementedInCoreOrUploadDownload)" /Parallel --logger:"trx;LogFileName=Ocaramba.Tests.netcoreapp.xml"
 
