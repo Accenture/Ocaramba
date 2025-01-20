9148139ed002c0ecb888759835870a435da02060
Jakub Raczek <jakub.raczek@accenture.com>
2025-01-20 12:00:12 +0100

    fixed docker

--- scripts/ExecutingTestsOnDockerGithubActions.ps1
+++ scripts/ExecutingTestsOnDockerGithubActions.ps1
@@ -1,7 +1,8 @@
 docker info
          
 docker ps -a
-docker exec -u ocaramba ocaramba_selenium bash -c "ls ; sed -i '/Documentation/,+5 d' ./Ocaramba.sln ; dotnet build ./Ocaramba.sln"
+docker exec -u root ocaramba_selenium bash -c "chmod -R 777 /Ocaramba"
+docker exec -u ocaramba ocaramba_selenium bash -c "ls ; sed -i '/Documentation/,+5 d' ./Ocaramba.sln ; sed -i '/CloudProviderCrossBrowser/,+5 d' ./Ocaramba.sln; dotnet build ./Ocaramba.sln"
 docker exec -u ocaramba ocaramba_selenium bash -c 'pwsh ./scripts/set_AppConfig_for_tests.ps1 "./Ocaramba.Tests.NUnit/bin/Debug/net8.0" "appsettings.Linux.json" "appSettings" "browser|PathToChromeDriverDirectory" "Chrome|/usr/local/bin/" -logValues -json'
 docker exec -u ocaramba ocaramba_selenium bash -c 'dotnet vstest ./Ocaramba.Tests.NUnit/bin/Debug/net8.0/Ocaramba.Tests.NUnit.dll /TestCaseFilter:"(TestCategory!=NotImplementedInCoreOrUploadDownload)" /Parallel --logger:trx;LogFileName=Ocaramba.Tests.Docker.trx' 
 docker exec -u ocaramba ocaramba_selenium bash -c "ls /Ocaramba/TestResults/ ; cp /Ocaramba/TestResults/*.trx /tmp/Ocaramba.Tests.Docker.trx"
