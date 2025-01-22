8b2bcc0a4ad9abda61983067bcdb9fd0601b9f7a
Jakub Raczek <jraczek@objectivity.co.uk>
2025-01-22 07:58:33 +0100

    selenium hub on docker (#509)
    * added docker hub
    
    * fixed path
    
    * fixed
    
    * fixed TestResults
    
    * fixed
    
    * rebuild
    
    * fix path
    
    * added build
    
    * fixed
    
    * fixed path
    
    * fixed build issue
    
    * fixed
    
    ---------
    
    Co-authored-by: Jakub Raczek <jakub.raczek@accenture.com>

--- scripts/ExecutingTestsOnWindowsGithubActions4.ps1
+++ scripts/ExecutingTestsOnWindowsGithubActions4.ps1
@@ -1,7 +1,7 @@
 echo '********************************************CloudProviderCrossBrowser tests********************************************'
         
 echo '********************************************BrowserStack tests********************************************'
-$filePath = ".\Ocaramba\Ocaramba.Tests.CloudProviderCrossBrowser\bin\Release\net8.0\browserstack.yml"
+$filePath = ".\Ocaramba.Tests.CloudProviderCrossBrowser\bin\Release\net8.0\browserstack.yml"
 
 $fileContent = Get-Content -Path $filePath
 
@@ -10,10 +10,10 @@ $fileContent = $fileContent -replace "BROWSERSTACKKEY", "`$($env:MAPPED_ENV_BROW
 
 # Write the updated content back to the file
 Set-Content -Path $filePath -Value $fileContent       
-.\Ocaramba\set_AppConfig_for_tests.ps1 ".\Ocaramba\Ocaramba.Tests.CloudProviderCrossBrowser\bin\Release\net8.0" "appsettings.json" "appSettings" "RemoteWebDriverHub" "https://$($env:MAPPED_ENV_BROWSERSTACKUSER):$($env:MAPPED_ENV_BROWSERSTACKKEY)@hub-cloud.browserstack.com/wd/hub" -json
-.\Ocaramba\set_AppConfig_for_tests.ps1 ".\Ocaramba\Ocaramba.Tests.CloudProviderCrossBrowser\bin\Release\net8.0" "appsettings.json" "DriverCapabilities" "buildName" "Ocaramba.Tests.BrowserStackCrossBrowser$($env:BuildVersion)" -logValues -json
+.\scripts\set_AppConfig_for_tests.ps1 ".\Ocaramba.Tests.CloudProviderCrossBrowser\bin\Release\net8.0" "appsettings.json" "appSettings" "RemoteWebDriverHub" "https://$($env:MAPPED_ENV_BROWSERSTACKUSER):$($env:MAPPED_ENV_BROWSERSTACKKEY)@hub-cloud.browserstack.com/wd/hub" -json
+.\scripts\set_AppConfig_for_tests.ps1 ".\Ocaramba.Tests.CloudProviderCrossBrowser\bin\Release\net8.0" "appsettings.json" "DriverCapabilities" "buildName" "Ocaramba.Tests.BrowserStackCrossBrowser$($env:BuildVersion)" -logValues -json
 
-dotnet vstest D:\a\Ocaramba\Ocaramba\Ocaramba\Ocaramba.Tests.CloudProviderCrossBrowser\bin\Release\net8.0\Ocaramba.Tests.CloudProviderCrossBrowser.dll /Logger:"trx;LogFileName=Ocaramba.Tests.BrowserStack.xml"
+dotnet vstest .\Ocaramba.Tests.CloudProviderCrossBrowser\bin\Release\net8.0\Ocaramba.Tests.CloudProviderCrossBrowser.dll /Logger:"trx;LogFileName=Ocaramba.Tests.BrowserStack.xml"
 
 if($lastexitcode -ne 0)
  {
--- scripts/ExecutingTestsOnWindowsGithubActions5.ps1
+++ scripts/ExecutingTestsOnWindowsGithubActions5.ps1
@@ -1,85 +1,25 @@
 echo '********************************************Downloading Selenium Grid********************************************'
 
-$url = $env:seleniumGridUrl
-$grid = $env:seleniumGridVersion
-$output = ".\$grid"
-$outputLogs = ".\"
+docker network create grid
 
-$start_time = Get-Date
+docker run -d -p 4442-4444:4442-4444 --net grid --name selenium-hub selenium/hub:latest
+docker run -d --net grid -e SE_EVENT_BUS_HOST=selenium-hub --shm-size="2g" -e SE_EVENT_BUS_PUBLISH_PORT=4442 -e SE_EVENT_BUS_SUBSCRIBE_PORT=4443  selenium/node-chrome:latest
 
-echo url: $url
-echo grid: $grid
-echo output: $output
-echo outputLogs: $outputLogs
+$Env:ASPNETCORE_ENVIRONMENT="Linux"
 
-echo "Downloading Selenium Grid from: $($url) to $($output)"
-Invoke-WebRequest -Uri "$($url)" -OutFile "$($output)"
-
-echo "Selenium Grid downloaded to:" $output
-
-echo "Time taken to download $($grid): $((Get-Date).Subtract($start_time).Seconds) second(s)"
-
-echo '******************************************Start Selenium Grid in background****************************************'
-
-java --version
-$firstIPAddress =  Get-NetIPConfiguration | Select-Object -ExpandProperty IPv4Address | Select-Object -First 1
-$firstIPAddress = $firstIPAddress.IPAddress
-Write-Output $firstIPAddress 
-$appHub=Start-Process java -ArgumentList '-jar', $output, 'hub' -RedirectStandardOutput "$outputLogs\console_hub.out" -RedirectStandardError "$outputLogs\console_hub.err" -PassThru
-
-Start-Sleep -s 5
-
-$retryCount = 6
-$delay = 10
-for ($i = 0; $i -lt $retryCount; $i++) {
-  try {
-    $response = Invoke-RestMethod -Uri "http://$($firstIPAddress):4444/wd/hub/status"
-    if ($response.ready) {
-      Write-Output "Selenium Grid is ready."
-      break
-    }
-  } catch {
-    Write-Output "Attempt $($i + 1) failed. Retrying in $delay seconds..."
-    Start-Sleep -Seconds $delay
-  }
-}
-if ($i -eq $retryCount) {
-  Write-Output  "Failed to connect to Selenium Grid after $retryCount attempts."
-}
-echo "Selenium Grid hub started"
-
-$appNode=Start-Process java -ArgumentList '-jar', $output, 'node --detect-drivers true' -RedirectStandardOutput "$outputLogs\console_node.out" -RedirectStandardError "$outputLogs\console_node.err" -PassThru
-
-Start-Sleep -s 5
-
-echo "Selenium Grid node started"
+echo $Env:ASPNETCORE_ENVIRONMENT
 
 echo '********************************************Run tests with Selenium Grid ****************************************'
 
-.\Ocaramba\set_AppConfig_for_tests.ps1 ".\Ocaramba\Ocaramba.Tests.NUnit\bin\Release\net8.0\" "appsettings.json" "appSettings" "browser|RemoteWebDriverHub" "RemoteWebDriver|http://$($firstIPAddress):4444/wd/hub" -json -logValues
+.\scripts\set_AppConfig_for_tests.ps1 ".\Ocaramba.Tests.NUnit\bin\Release\net8.0\" "appsettings.Linux.json" "appSettings" "browser|RemoteWebDriverHub" "RemoteWebDriver|http://localhost:4444/wd/hub" -json -logValues
 
-dotnet vstest .\Ocaramba\Ocaramba.Tests.NUnit\bin\Release\net8.0\Ocaramba.Tests.NUnit.dll /TestCaseFilter:"TestCategory=Grid" /Parallel /Logger:"trx;LogFileName=Ocaramba.Tests.NUnitGrid.xml"
+dotnet vstest .\Ocaramba.Tests.NUnit\bin\Release\net8.0\Ocaramba.Tests.NUnit.dll /TestCaseFilter:"TestCategory=Grid" /Parallel /Logger:"trx;LogFileName=Ocaramba.Tests.NUnitGrid.xml"
 
 echo '*****************************Run CloudProviderCrossBrowser tests with Selenium Grid****************************'
 
-.\Ocaramba\set_AppConfig_for_tests.ps1 ".\Ocaramba\Ocaramba.Tests.CloudProviderCrossBrowser\bin\Release\net8.0" "appsettings.json" "appSettings" "RemoteWebDriverHub" "http://$($firstIPAddress):4444/wd/hub" -json -logValues
+.\scripts\set_AppConfig_for_tests.ps1 ".\Ocaramba.Tests.CloudProviderCrossBrowser\bin\Release\net8.0" "appsettings.Linux.json" "appSettings" "RemoteWebDriverHub" "http://localhost:4444/wd/hub" -json -logValues
 
-dotnet vstest .\Ocaramba\Ocaramba.Tests.CloudProviderCrossBrowser\bin\Release\net8.0\Ocaramba.Tests.CloudProviderCrossBrowser.dll /TestCaseFilter:"FullyQualifiedName~Chrome" /Parallel /Logger:"trx;LogFileName=Ocaramba.Tests.CloudProviderCrossBrowserGrid.xml"
-
-if($lastexitcode -ne 0)
-{
-  echo 'lastexitcode' $lastexitcode
-}
-
-echo '*****************************Stop Selenium Grid****************************'
-
-echo "Stop Selenium Grid node"
-
-Stop-Process -Id $appNode.Id
-
-echo "Stop Selenium Grid hub"
-
-Stop-Process -Id $appHub.Id
+dotnet vstest .\Ocaramba.Tests.CloudProviderCrossBrowser\bin\Release\net8.0\Ocaramba.Tests.CloudProviderCrossBrowser.dll /TestCaseFilter:"FullyQualifiedName~Chrome" /Parallel /Logger:"trx;LogFileName=Ocaramba.Tests.CloudProviderCrossBrowserGrid.xml"
 
 if($lastexitcode -ne 0)
 {
