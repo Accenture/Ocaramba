91781b92be48c1c951b6bfb8fec22c2b3d402a42
Jakub Raczek <jakub.raczek@accenture.com>
2025-06-10 07:58:48 +0200

    Remove obsolete net472 test execution from GitHub Actions script

--- scripts/ExecutingTestsOnWindowsGithubActions1.ps1
+++ scripts/ExecutingTestsOnWindowsGithubActions1.ps1
@@ -8,11 +8,6 @@ echo '********************************************net8.0 tests******************
 
 dotnet vstest .\Ocaramba.Tests.Angular\bin\Release\net8.0\Ocaramba.Tests.Angular.dll .\Ocaramba.Tests.NUnit\bin\Release\net8.0\Ocaramba.Tests.NUnit.dll /TestCaseFilter:"(TestCategory!=TakingScreehShots)" /Parallel /Logger:"trx;LogFileName=Ocaramba.Tests.netcoreapp.trx"
 
-echo '********************************************net472 tests********************************************'
-
-#.\scripts\set_AppConfig_for_tests.ps1 "D:\a\Ocaramba\Ocaramba\Ocaramba\Ocaramba.Tests.Features\bin\Release\net472" "Ocaramba.Tests.Features.dll.config" "appsettings.json" "browser|PathToChromeDriverDirectory" "Chrome|C:\SeleniumWebDrivers\ChromeDriver" -logValues
-
-#vstest.console.exe D:\a\Ocaramba\Ocaramba\Ocaramba\Ocaramba.Tests.Features\bin\Release\net472\Ocaramba.Tests.Features.dll /Logger:"trx;LogFileName=Ocaramba.Tests.Features.trx"
 if($lastexitcode -ne 0)
  {
   echo 'lastexitcode' $lastexitcode
