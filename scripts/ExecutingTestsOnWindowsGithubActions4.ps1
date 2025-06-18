0b0c0d4a21e32a6cf5c72f08ada231177448ea73
Jakub Raczek <jakub.raczek@gmail.com>
2025-06-18 20:16:00 +0000

    fixed

--- scripts/ExecutingTestsOnWindowsGithubActions4.ps1
+++ scripts/ExecutingTestsOnWindowsGithubActions4.ps1
@@ -1,8 +1,7 @@
 echo '********************************************CloudProviderCrossBrowser tests********************************************'
         
 echo '********************************************BrowserStack tests********************************************'
-$filePath = ".\Ocaramba.Tests.CloudProviderCrossBrowser\bin\Debug\net8.0\browserstack.yml"
-dotnet build
+$filePath = ".\Ocaramba.Tests.CloudProviderCrossBrowser\bin\Release\net8.0\browserstack.yml"
 $fileContent = Get-Content -Path $filePath
 
 $fileContent = $fileContent -replace "BROWSERSTACKUSER", "`$($env:MAPPED_ENV_BROWSERSTACKUSER)"
@@ -10,7 +9,7 @@ $fileContent = $fileContent -replace "BROWSERSTACKKEY", "`$($env:MAPPED_ENV_BROW
 
 # Write the updated content back to the file
 
-dotnet vstest ./Ocaramba.Tests.CloudProviderCrossBrowser/bin/Debug/net8.0/Ocaramba.Tests.CloudProviderCrossBrowser.dll /Logger:"trx;LogFileName=Ocaramba.Tests.BrowserStack.xml"
+dotnet vstest ./Ocaramba.Tests.CloudProviderCrossBrowser/bin/Release/net8.0/Ocaramba.Tests.CloudProviderCrossBrowser.dll /Logger:"trx;LogFileName=Ocaramba.Tests.BrowserStack.xml"
 if($lastexitcode -ne 0)
  {
   echo 'lastexitcode' $lastexitcode
