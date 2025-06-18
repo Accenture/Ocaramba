7092e36a44d9bfdab607b7aa866077710591d347
Jakub Raczek <jakub.raczek@gmail.com>
2025-06-18 16:33:51 +0200

    Update ExecutingTestsOnWindowsGithubActions4.ps1

--- scripts/ExecutingTestsOnWindowsGithubActions4.ps1
+++ scripts/ExecutingTestsOnWindowsGithubActions4.ps1
@@ -2,13 +2,14 @@ echo '********************************************CloudProviderCrossBrowser test
         
 echo '********************************************BrowserStack tests********************************************'
 $filePath = ".\Ocaramba.Tests.CloudProviderCrossBrowser\bin\Release\net8.0\browserstack.yml"
-
+dotnet build
 $fileContent = Get-Content -Path $filePath
 
 $fileContent = $fileContent -replace "BROWSERSTACKUSER", "`$($env:MAPPED_ENV_BROWSERSTACKUSER)"
 $fileContent = $fileContent -replace "BROWSERSTACKKEY", "`$($env:MAPPED_ENV_BROWSERSTACKKEY)"
 
 # Write the updated content back to the file
+
 dotnet test --logger:"nunit;LogFilePath=./TestResults/Ocaramba.Tests.BrowserStack.xml" --results-directory ./TestResults
 if($lastexitcode -ne 0)
  {
