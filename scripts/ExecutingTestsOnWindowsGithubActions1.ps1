7c0f2d9c49824e68be5bd32423306db639df2833
Jakub Raczek <jakub.raczek@accenture.com>
2025-06-20 08:05:59 +0200

    remove subprojects

--- scripts/ExecutingTestsOnWindowsGithubActions1.ps1
+++ scripts/ExecutingTestsOnWindowsGithubActions1.ps1
@@ -8,6 +8,8 @@ echo '********************************************net8.0 tests******************
 
 dotnet vstest .\Ocaramba.Tests.Angular\bin\Release\net8.0\Ocaramba.Tests.Angular.dll .\Ocaramba.Tests.NUnit\bin\Release\net8.0\Ocaramba.Tests.NUnit.dll /TestCaseFilter:"(TestCategory!=TakingScreehShots)" /Parallel /Logger:"trx;LogFileName=Ocaramba.Tests.netcoreapp.xml"
 
+dotnet vstest .\Ocaramba.Tests.NUnitExtentReports\bin\Release\net8.0\Ocaramba.Tests.NUnitExtentReports.dll /TestCaseFilter:"(TestCategory!=TakingScreehShots)" /Parallel /Logger:"trx;LogFileName=Ocaramba.Tests.NUnitExtentReports.xml"
+
 if($lastexitcode -ne 0)
  {
   echo 'lastexitcode' $lastexitcode
