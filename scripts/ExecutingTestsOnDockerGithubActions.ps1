24864a8ce49620bd4a11da56ba8f0336729a8fc5
Jakub Raczek <jakub.raczek@accenture.com>
2025-06-24 08:58:43 +0200

    Fix path for copying test output in Docker GitHub Actions script

--- scripts/ExecutingTestsOnDockerGithubActions.ps1
+++ scripts/ExecutingTestsOnDockerGithubActions.ps1
@@ -5,7 +5,7 @@ docker exec -u root ocaramba_selenium bash -c "chmod -R 777 /Ocaramba"
 docker exec -u ocaramba ocaramba_selenium bash -c "ls ; sed -i '/Documentation/,+5 d' ./Ocaramba.sln ; sed -i '/CloudProviderCrossBrowser/,+5 d' ./Ocaramba.sln; dotnet build ./Ocaramba.sln"
 docker exec -u ocaramba ocaramba_selenium bash -c 'pwsh ./scripts/set_AppConfig_for_tests.ps1 "./Ocaramba.Tests.NUnit/bin/Debug/net8.0" "appsettings.Linux.json" "appSettings" "browser|PathToChromeDriverDirectory" "Chrome|/usr/local/bin/" -logValues -json'
 docker exec -u ocaramba ocaramba_selenium bash -c 'dotnet vstest ./Ocaramba.Tests.NUnit/bin/Debug/net8.0/Ocaramba.Tests.NUnit.dll /TestCaseFilter:"(TestCategory!=NotImplementedInCoreOrUploadDownload)" /Parallel --logger:trx;LogFileName=Ocaramba.Tests.Docker.trx' 
-docker exec -u ocaramba ocaramba_selenium bash -c "cp ./Ocaramba.Tests.NUnit/bin/Debug/net8.0/. ./tmp/TestOutput"
+docker exec -u ocaramba ocaramba_selenium bash -c "cp -r /Ocaramba.Tests.NUnit/bin/Debug/net8.0/TestOutput/. /tmp/TestOutput"
 docker exec -u ocaramba ocaramba_selenium bash -c "ls /Ocaramba/TestResults/ && cp /Ocaramba/TestResults/*.trx /tmp/Ocaramba.Tests.Docker.trx"
 docker cp ocaramba_selenium:/tmp/Ocaramba.Tests.Docker.trx .
 docker rm ocaramba_selenium --force
