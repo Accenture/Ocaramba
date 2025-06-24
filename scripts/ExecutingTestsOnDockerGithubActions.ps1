fd961fb8daaadfca56f75946cdf4454666f7593f
Jakub Raczek <jakub.raczek@accenture.com>
2025-06-24 10:47:14 +0200

    Fix typo in Docker command for copying test output

--- scripts/ExecutingTestsOnDockerGithubActions.ps1
+++ scripts/ExecutingTestsOnDockerGithubActions.ps1
@@ -6,7 +6,7 @@ docker exec -u ocaramba ocaramba_selenium bash -c "ls ; sed -i '/Documentation/,
 docker exec -u ocaramba ocaramba_selenium bash -c 'pwsh ./scripts/set_AppConfig_for_tests.ps1 "./Ocaramba.Tests.NUnit/bin/Debug/net8.0" "appsettings.Linux.json" "appSettings" "browser|PathToChromeDriverDirectory" "Chrome|/usr/local/bin/" -logValues -json'
 docker exec -u ocaramba ocaramba_selenium bash -c 'dotnet vstest ./Ocaramba.Tests.NUnit/bin/Debug/net8.0/Ocaramba.Tests.NUnit.dll /TestCaseFilter:"(TestCategory!=NotImplementedInCoreOrUploadDownload)" /Parallel --logger:trx;LogFileName=Ocaramba.Tests.Docker.trx' 
 docker exec -u ocaramba ocaramba_selenium bash -c "mkdir -p /tmp/TestOutput && ls ./Ocaramba.Tests.NUnit/bin/Debug/net8.0/TestOutput/ && cp -r ./Ocaramba.Tests.NUnit/bin/Debug/net8.0/TestOutput/. /tmp/TestOutput"
-docker cp -r ocaramba_selenium:/tmp/TestOutput. ./TestOutput
+docker cp -r ocaramba_selenium:/tmp/TestOutput/. ./TestOutput
 docker exec -u ocaramba ocaramba_selenium bash -c "ls /Ocaramba/TestResults/ && cp /Ocaramba/TestResults/*.trx /tmp/Ocaramba.Tests.Docker.trx"
 docker cp ocaramba_selenium:/tmp/Ocaramba.Tests.Docker.trx .
 docker rm ocaramba_selenium --force
