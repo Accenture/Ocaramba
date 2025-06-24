ecbbe6fae370b1ef569807cb4501d2c5fb15f4cc
Jakub Raczek <jakub.raczek@accenture.com>
2025-06-24 08:44:47 +0200

    Refactor GitHub Actions scripts to streamline test result handling and improve path references

--- scripts/ExecutingTestsOnWindowsGithubActions5.ps1
+++ scripts/ExecutingTestsOnWindowsGithubActions5.ps1
@@ -48,7 +48,7 @@ echo '*****************************Run CloudProviderCrossBrowser tests with Sele
 
 .\scripts\set_AppConfig_for_tests.ps1 ".\Ocaramba.Tests.CloudProviderCrossBrowser\bin\Release\net8.0" "appsettings.Linux.json" "appSettings" "RemoteWebDriverHub" "http://localhost:4444/wd/hub" -json -logValues
 
-dotnet vstest ./Ocaramba.Tests.CloudProviderCrossBrowser/bin/Release/net8.0/Ocaramba.Tests.CloudProviderCrossBrowser.dll /TestCaseFilter:"FullyQualifiedName~Chrome" /Parallel /Logger:"trx;LogFileName=Ocaramba.Tests.CloudProviderCrossBrowserGrid.xml"
+dotnet vstest ./Ocaramba.Tests.CloudProviderCrossBrowser/bin/Release/net8.0/Ocaramba.Tests.CloudProviderCrossBrowser.dll /Parallel /Logger:"trx;LogFileName=Ocaramba.Tests.CloudProviderCrossBrowserGrid.xml"
 
 $staging = "TempZipStaging"
 New-Item -ItemType Directory -Path $staging -Force
