82a089d98adfba5286f105505f33d846eef97f26
Jakub Raczek <jakub.raczek@accenture.com>
2025-06-25 16:13:11 +0200

    Fix vstest command to include /Parallel flag for improved test execution

--- scripts/ExecutingTestsOnWindowsGithubActions6.ps1
+++ scripts/ExecutingTestsOnWindowsGithubActions6.ps1
@@ -6,7 +6,7 @@ echo '********************************************saucelabs tests***************
     
 .\scripts\set_AppConfig_for_tests.ps1 ".\Ocaramba.Tests.CloudProviderCrossBrowser\bin\Release\net8.0" "appsettings.json" "DriverCapabilities" "buildName" "Ocaramba.Tests.SauceLabsCrossBrowser$($env:BuildVersion)" -logValues -json
     
-dotnet vstest .\Ocaramba.Tests.CloudProviderCrossBrowser\bin\Release\net8.0\Ocaramba.Tests.CloudProviderCrossBrowser.dll  /Logger:"trx;LogFileName=Ocaramba.Tests.saucelabsnet8.xml"
+dotnet vstest ./Ocaramba.Tests.CloudProviderCrossBrowser/bin/Release/net8.0/Ocaramba.Tests.CloudProviderCrossBrowser.dll /Parallel /Logger:"trx;LogFileName=Ocaramba.Tests.saucelabsnet8.xml"
 
 Compress-Archive -Path "./Ocaramba.Tests.CloudProviderCrossBrowser/bin/Release/net8.0/TestOutput/*" -DestinationPath "./Ocaramba.Tests.CloudProviderCrossBrowser/bin/Release/net8.0/ExecutingTestsOnsaucelabLinux$env:GITHUB_RUN_ID.zip"
 
