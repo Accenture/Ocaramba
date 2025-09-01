a0cfd2889f433bf3c877df729ee9c7e512e20acf
Jakub Raczek <jakub.raczek@gmail.com>
2025-09-01 05:53:58 +0000

    Remove sensitive information handling from BrowserStack test scripts

--- scripts/ExecutingTestsOnLinuxBrowserStackGithubActions.ps1
+++ scripts/ExecutingTestsOnLinuxBrowserStackGithubActions.ps1
@@ -5,16 +5,6 @@ $Env:ASPNETCORE_ENVIRONMENT="Linux"
 
 echo $Env:ASPNETCORE_ENVIRONMENT
 
-$filePath = ".\Ocaramba.Tests.BrowserStack\bin\Release\net8.0\browserstack.yml"
-
-$fileContent = Get-Content -Path $filePath
-
-$fileContent = $fileContent -replace "BROWSERSTACK_USERNAME", "`$($env:MAPPED_ENV_BROWSERSTACKUSER)"
-$fileContent = $fileContent -replace "BROWSERSTACK_ACCESS_KEY", "`$($env:MAPPED_ENV_BROWSERSTACKKEY)"
-
-# Write the updated content back to the file
-Set-Content -Path $filePath -Value $fileContent
-
 dotnet vstest ./Ocaramba.Tests.BrowserStack/bin/Release/net8.0/Ocaramba.Tests.BrowserStack.dll /Logger:"trx;LogFileName=Ocaramba.Tests.BrowserStacknetcoreapp.xml"
 
 Compress-Archive -Path "./Ocaramba.Tests.BrowserStack/bin/Release/net8.0/TestOutput/*" -DestinationPath "./Ocaramba.Tests.BrowserStack/bin/Release/net8.0/ExecutingTestsOnBrowserStackLinux$env:GITHUB_RUN_ID.zip"
--- scripts/ExecutingTestsOnWindowsGithubActions4.ps1
+++ scripts/ExecutingTestsOnWindowsGithubActions4.ps1
@@ -1,14 +1,6 @@
 echo '********************************************CloudProviderCrossBrowser tests********************************************'
         
 echo '********************************************BrowserStack tests********************************************'
-$filePath = ".\Ocaramba.Tests.BrowserStack\bin\Release\net8.0\browserstack.yml"
-$fileContent = Get-Content -Path $filePath
-
-$fileContent = $fileContent -replace "BROWSERSTACK_USERNAME", "`$($env:MAPPED_ENV_BROWSERSTACKUSER)"
-$fileContent = $fileContent -replace "BROWSERSTACK_ACCESS_KEY", "`$($env:MAPPED_ENV_BROWSERSTACKKEY)"
-
-# Write the updated content back to the file
-Set-Content -Path $filePath -Value $fileContent
 
 dotnet vstest ./Ocaramba.Tests.BrowserStack/bin/Release/net8.0/Ocaramba.Tests.BrowserStack.dll /Logger:"trx;LogFileName=Ocaramba.Tests.BrowserStack.xml"
 
