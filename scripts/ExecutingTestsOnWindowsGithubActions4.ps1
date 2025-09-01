6211611845e1d6a428aa744123a5666272bb2d0e
Jakub Raczek <jakub.raczek@gmail.com>
2025-09-01 07:55:14 +0200

    Fix browser stack (#588)
    * Refactor BrowserStack credentials in GitHub Actions and scripts to use environment variables for improved security
    
    * Remove sensitive information handling from BrowserStack test scripts

--- scripts/ExecutingTestsOnWindowsGithubActions4.ps1
+++ scripts/ExecutingTestsOnWindowsGithubActions4.ps1
@@ -1,13 +1,6 @@
 echo '********************************************CloudProviderCrossBrowser tests********************************************'
         
 echo '********************************************BrowserStack tests********************************************'
-$filePath = ".\Ocaramba.Tests.BrowserStack\bin\Release\net8.0\browserstack.yml"
-$fileContent = Get-Content -Path $filePath
-
-$fileContent = $fileContent -replace "BROWSERSTACKUSER", "`$($env:MAPPED_ENV_BROWSERSTACKUSER)"
-$fileContent = $fileContent -replace "BROWSERSTACKKEY", "`$($env:MAPPED_ENV_BROWSERSTACKKEY)"
-
-# Write the updated content back to the file
 
 dotnet vstest ./Ocaramba.Tests.BrowserStack/bin/Release/net8.0/Ocaramba.Tests.BrowserStack.dll /Logger:"trx;LogFileName=Ocaramba.Tests.BrowserStack.xml"
 
