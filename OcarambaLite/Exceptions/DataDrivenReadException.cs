93267e9b3cd9138d06526a04fef6b9d267d16658
Jakub Raczek <jakub.raczek@accenture.com>
2025-09-02 10:34:13 +0200

    fixed code warnings

--- OcarambaLite/Exceptions/DataDrivenReadException.cs
+++ OcarambaLite/Exceptions/DataDrivenReadException.cs
@@ -23,7 +23,6 @@
 namespace Ocaramba.Exceptions
 {
     using System;
-    using System.Runtime.Serialization;
 
     /// <summary>
     /// Exception to throw when problem with setting the test case name from parameters.
@@ -53,16 +52,6 @@ namespace Ocaramba.Exceptions
         /// <param name="innerException">The exception that is the cause of the current exception, or a null reference (Nothing in Visual Basic) if no inner exception is specified.</param>
         public DataDrivenReadException(string message, Exception innerException)
             : base(message, innerException)
-        {
-        }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="DataDrivenReadException"/> class.
-        /// </summary>
-        /// <param name="info">The <see cref="T:System.Runtime.Serialization.SerializationInfo" /> that holds the serialized object data about the exception being thrown.</param>
-        /// <param name="context">The <see cref="T:System.Runtime.Serialization.StreamingContext" /> that contains contextual information about the source or destination.</param>
-        protected DataDrivenReadException(SerializationInfo info, StreamingContext context)
-            : base(info, context)
         {
         }
     }
