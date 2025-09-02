6409653cf55f5515c153ce8b41606d265a7b19e7
Jakub Raczek <jakub.raczek@accenture.com>
2025-09-02 09:14:46 +0200

    change  test execution

--- scripts/ExecutingTestsOnWindowsGithubActions2.ps1
+++ scripts/ExecutingTestsOnWindowsGithubActions2.ps1
@@ -6,20 +6,14 @@ echo '********************************************net8.0 tests******************
 .\scripts\set_AppConfig_for_tests.ps1 ".\Ocaramba.Tests.Xunit\bin\Release\net8.0" "appsettings.json" "appSettings" "browser" "Edge" -logValues -json 
 .\scripts\set_AppConfig_for_tests.ps1 ".\Ocaramba.Tests.MsTest\bin\Release\net8.0" "appsettings.json" "appSettings" "browser" "Edge" -logValues -json
 .\scripts\set_AppConfig_for_tests.ps1 ".\Ocaramba.UnitTests\bin\Release\net8.0" "appsettings.json" "appSettings" "browser" "Edge" -logValues -json
+.\scripts\set_AppConfig_for_tests.ps1 ".\Ocaramba.Tests.NUnit\bin\Release\net8.0" "appsettings.json" "appSettings" "browser" "EdgeChromium|C:\SeleniumWebDrivers\EdgeDriver" -logValues -json
 
-dotnet vstest .\Ocaramba.Tests.Features\bin\Release\net8.0\Ocaramba.Tests.Features.dll /TestCaseFilter:"(TestCategory!=TakingScreehShots)" /Parallel /Logger:"trx;LogFileName=Ocaramba.Tests.Features.xml"
+dotnet vstest .\Ocaramba.Tests.Features\bin\Release\net8.0\Ocaramba.Tests.Features.dll .\Ocaramba.Tests.Xunit\bin\Release\net8.0\Ocaramba.Tests.Xunit.dll .\Ocaramba.Tests.MsTest\bin\Release\net8.0\Ocaramba.Tests.MsTest.dll .\Ocaramba.Tests.MsTest\bin\Release\net8.0\Ocaramba.Tests.MsTest.dll  /TestCaseFilter:"(TestCategory!=TakingScreehShots)" /Parallel /Logger:"trx;LogFileName=Ocaramba.Tests.Features.xml"
 
-dotnet vstest .\Ocaramba.Tests.Xunit\bin\Release\net8.0\Ocaramba.Tests.Xunit.dll  /TestCaseFilter:"(TestCategory!=TakingScreehShots)" /Parallel /Logger:"trx;LogFileName=Ocaramba.Tests.Xunit.xml"
 
-dotnet vstest .\Ocaramba.Tests.MsTest\bin\Release\net8.0\Ocaramba.Tests.MsTest.dll  /TestCaseFilter:"(TestCategory!=TakingScreehShots)" /Parallel /Logger:"trx;LogFileName=Ocaramba.Tests.MsTest.xml"
-
-dotnet vstest .\Ocaramba.UnitTests\bin\Release\net8.0\Ocaramba.UnitTests.dll  /Parallel /Logger:"trx;LogFileName=Ocaramba.Tests.UnitTests.xml"
 
 
 echo '********************************************EdgeChrominum tests********************************************'
-
-.\scripts\set_AppConfig_for_tests.ps1 ".\Ocaramba.Tests.NUnit\bin\Release\net8.0" "appsettings.json" "appSettings" "browser" "EdgeChromium|C:\SeleniumWebDrivers\EdgeDriver" -logValues -json
-        
 
 dotnet vstest .\Ocaramba.Tests.NUnit\bin\Release\net8.0\Ocaramba.Tests.NUnit.dll /TestCaseFilter:"(TestCategory=Grid)" /Parallel /Logger:"trx;LogFileName=Ocaramba.Tests.EdgeChrominum.xml"
 
