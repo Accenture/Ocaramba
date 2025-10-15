8c39de1c7471bef3afdc2dc5dc720fd88e1578df
Jakub Raczek <jakub.raczek@gmail.com>
2025-10-15 09:16:16 +0000

    Refactor script to change directory before executing BrowserStack tests

--- scripts/ExecutingTestsOnLinuxBrowserStackGithubActions.ps1
+++ scripts/ExecutingTestsOnLinuxBrowserStackGithubActions.ps1
@@ -4,8 +4,8 @@ Write-Output '********************************************BrowserStack tests****
 $Env:ASPNETCORE_ENVIRONMENT="Linux"
 
 Write-Output $Env:ASPNETCORE_ENVIRONMENT
-
-dotnet browserstack-sdk --no-build  ./Ocaramba.Tests.BrowserStack/bin/Release/net8.0/Ocaramba.Tests.BrowserStack.dll /Logger:"trx;LogFileName=Ocaramba.Tests.BrowserStacknetcoreapp.xml"
+cd Ocaramba.Tests.BrowserStack/bin/Release/net8.0/
+dotnet browserstack-sdk --no-build  Ocaramba.Tests.BrowserStack.dll /Logger:"trx;LogFileName=Ocaramba.Tests.BrowserStacknetcoreapp.xml"
 
 Compress-Archive -Path "./Ocaramba.Tests.BrowserStack/bin/Release/net8.0/TestOutput/*" -DestinationPath "./Ocaramba.Tests.BrowserStack/bin/Release/net8.0/ExecutingTestsOnBrowserStackLinux$env:GITHUB_RUN_ID.zip"
 			  
