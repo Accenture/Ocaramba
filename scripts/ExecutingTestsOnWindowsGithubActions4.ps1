050577cccec125e41249591ea590808b6cb7a4d1
Jakub Raczek <jakub.raczek@gmail.com>
2025-10-27 14:30:04 +0000

    Update Appium.WebDriver dependency to version 8.0.1 and enhance test output handling in GitHub Actions script

--- scripts/ExecutingTestsOnWindowsGithubActions4.ps1
+++ scripts/ExecutingTestsOnWindowsGithubActions4.ps1
@@ -5,6 +5,8 @@ cd Ocaramba.Tests.BrowserStack/bin/Release/net8.0/
 
 dotnet browserstack-sdk Ocaramba.Tests.BrowserStack.dll /Logger:"trx;LogFileName=Ocaramba.Tests.BrowserStack.xml"
 
+Copy-Item -Path "*.log" -Destination "./TestOutput/" -Recurse
+Compress-Archive -Path "./TestOutput/*" -DestinationPath "./ExecutingTestsOnBrowserStackWindows$env:GITHUB_RUN_ID.zip"
 if($lastexitcode -ne 0)
  {
   echo 'lastexitcode' $lastexitcode
