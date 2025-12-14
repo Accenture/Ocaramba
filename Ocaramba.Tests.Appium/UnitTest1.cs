16328d639d5388ff3effc050d20ee97909ded813
Jakub Raczek <jakub.raczek@gmail.com>
2025-12-14 13:19:41 +0000

    updated test

--- Ocaramba.Tests.Appium/UnitTest1.cs
+++ Ocaramba.Tests.Appium/UnitTest1.cs
@@ -16,6 +16,17 @@ namespace Ocaramba.Tests.Appium
             Assert.That(page.IsSomeElementPresent(), Is.True);
             Assert.That(page.IsPreferencePresent(), Is.True);
             page.ClickPreference();
+            driver.Navigate().Back();
+            page.ClickViews();
+            page.ClickWebView();
+            this.driverContext.SwitchToWebView();
+            var webViewElement = this.DriverContext.Driver.FindElement(By.CssSelector("h1"));
+            Assert.That(webViewElement.Text.Contains("This page is a Selenium sandbox"), Is.True);
+            this.driverContext.SwitchToNativeApp();
+            driver.Navigate().Back();
+            driver.Navigate().Back();
+            Assert.That(page.IsPreferencePresent(), Is.True);
+            page.ClickPreference();
         }
     }
 }
